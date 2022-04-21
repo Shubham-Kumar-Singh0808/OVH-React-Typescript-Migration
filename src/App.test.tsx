a5dd2e82c9c072b688db57f7354d3b63ca4b9773
Mohd Basheer <mohd.basheer@raybiztech.com>
2022-04-21 17:32:49 +0530

    Removed all the files
    Removed all the files

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('App should render without crashing', async () => {
-  render(<App />)
-  const linkElement = screen.getByText(/Loading.../i)
-  expect(await linkElement).toBeInTheDocument()
-})
