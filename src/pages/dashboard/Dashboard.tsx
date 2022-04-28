977428c394933b1b22f53fa3fbad313abd4fe236
paulryan <paulryanlucero@gmail.com>
2022-04-28 18:13:17 +0800

    removed userROlesPermission temporarily because it's empty.

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,6 +1,5 @@
 import OCard from '../../components/ReusableComponent/OCard'
 import React from 'react'
-import TestComponent from '../../views/blank/TestComponent'
 
 const Dashboard = (): JSX.Element => {
   return (
