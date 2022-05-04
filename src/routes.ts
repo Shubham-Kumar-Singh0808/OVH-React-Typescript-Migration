0157146f4aa803e065c8179969d002fab6f9a02d
Sai Kishore <saikishore.guguloth@raybiztech.com>
2022-05-04 16:43:53 +0530

    Data Integration for General Information and Re-usable component for Family Details and Employee Skill

--- src/routes.ts
+++ src/routes.ts
@@ -9,7 +9,7 @@ export type route = {
 }
 
 // examples
-const Dashboard = React.lazy(() => import('./pages/dashboard/Dashboard'))
+const Dashboard = React.lazy(() => import('./pages/Dashboard/Dashboard'))
 
 const PersonalInfoTab = React.lazy(
   () => import('./pages/MyProfile/PersonalInfoTab/PersonalInfoTab'),
