e3a8f29ca700c9406b32bbcbce63886c01c34adc
vinesh <vineshmerugu1@gmail.com>
2022-06-10 15:16:37 +0530

    added Pagination

--- src/pages/MyProfile/ReviewsTab/EmployeeReviews.tsx
+++ src/pages/MyProfile/ReviewsTab/EmployeeReviews.tsx
@@ -8,10 +8,16 @@ import {
   CCardHeader,
   CCardBody,
   CBadge,
+  CRow,
+  CCol,
 } from '@coreui/react-pro'
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { useAppDispatch, useTypedSelector } from '../../../stateStore'
 import { reduxServices } from '../../../reducers/reduxServices'
+import OPageSizeSelect from '../../../components/ReusableComponent/OPageSizeSelect'
+import OPagination from '../../../components/ReusableComponent/OPagination'
+import { currentPageData } from '../../../utils/paginationUtils'
+import { usePagination } from '../../../middleware/hooks/usePagination'
 const EmployeeReviews = (): JSX.Element => {
   const employeeId = useTypedSelector(
     reduxServices.authentication.selectors.selectEmployeeId,
@@ -27,6 +33,34 @@ const EmployeeReviews = (): JSX.Element => {
     )
   }, [dispatch, employeeId])
 
+  const {
+    paginationRange,
+    setPageSize,
+    setCurrentPage,
+    currentPage,
+    pageSize,
+  } = usePagination(getReviewDetails.length, 20)
+
+  useEffect(() => {
+    setPageSize(20)
+  }, [getReviewDetails, setPageSize, setCurrentPage])
+
+  const handlePageSizeSelectChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setPageSize(Number(event.target.value))
+    setCurrentPage(1)
+  }
+
+  const getItemNumber = (index: number) => {
+    return (currentPage - 1) * pageSize + index + 1
+  }
+
+  const currentPageItems = useMemo(
+    () => currentPageData(getReviewDetails, currentPage, pageSize),
+    [getReviewDetails, currentPage, pageSize],
+  )
+
   return (
     <>
       <CCardHeader>
@@ -34,63 +68,123 @@ const EmployeeReviews = (): JSX.Element => {
       </CCardHeader>
       <br></br>
       <CCardBody className="ps-0 pe-0">
-        <CTable striped>
-          <CTableHead>
-            <CTableRow>
-              <CTableHeaderCell scope="col">#</CTableHeaderCell>
-              <CTableHeaderCell scope="col">ID</CTableHeaderCell>
-              <CTableHeaderCell scope="col">Employee Name</CTableHeaderCell>
-              <CTableHeaderCell scope="col">Manager Name</CTableHeaderCell>
-              <CTableHeaderCell scope="col">Department</CTableHeaderCell>
-              <CTableHeaderCell scope="col">Designation</CTableHeaderCell>
-              <CTableHeaderCell scope="col">Month</CTableHeaderCell>
-              <CTableHeaderCell scope="col">Emp Avg Rating</CTableHeaderCell>
-              <CTableHeaderCell scope="col">
-                Managers Avg Rating
-              </CTableHeaderCell>
-              <CTableHeaderCell scope="col">Status</CTableHeaderCell>
-            </CTableRow>
-          </CTableHead>
-          <CTableBody>
-            {getReviewDetails?.map((review, index) => (
-              <CTableRow key={index}>
-                <CTableDataCell scope="row">{index + 1}</CTableDataCell>
-                <CTableDataCell scope="row">{review.id}</CTableDataCell>
-                <CTableDataCell scope="row">
-                  {review.employeeName}
-                </CTableDataCell>
-                <CTableDataCell scope="row">
-                  {review.manager1Name}
-                </CTableDataCell>
-                <CTableDataCell scope="row">
-                  {review.empDepartmentName}
-                </CTableDataCell>
-                <CTableDataCell scope="row">
-                  {review.empDesignationName}
-                </CTableDataCell>
-                <CTableDataCell scope="row">
-                  {review.cycleStartDate}
-                </CTableDataCell>
-                <CTableDataCell scope="row">
-                  {review.empAvgRating}
-                </CTableDataCell>
-                <CTableDataCell scope="row">
-                  {review.overallAvgRating || 'N/A'}
-                </CTableDataCell>
-                <CTableDataCell scope="row">
-                  <CBadge color="success"> {review.formStatus}</CBadge>
-                </CTableDataCell>
-              </CTableRow>
-            ))}
-          </CTableBody>
-        </CTable>
+        {getReviewDetails.length ? (
+          <>
+            <CTable striped>
+              <CTableHead>
+                <CTableRow>
+                  <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">ID</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Employee Name</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Manager Name</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Department</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Designation</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Month</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">
+                    Emp Avg Rating
+                  </CTableHeaderCell>
+                  <CTableHeaderCell scope="col">
+                    Managers Avg Rating
+                  </CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Status</CTableHeaderCell>
+                </CTableRow>
+              </CTableHead>
+              <CTableBody>
+                {currentPageItems.map((review, index) => {
+                  return (
+                    <CTableRow key={index}>
+                      <CTableDataCell scope="row">
+                        {getItemNumber(index)}
+                      </CTableDataCell>
+                      <CTableDataCell scope="row">{review.id}</CTableDataCell>
+                      <CTableDataCell scope="row">
+                        {review.employeeName}
+                      </CTableDataCell>
+                      <CTableDataCell scope="row">
+                        {review.manager1Name}
+                      </CTableDataCell>
+                      <CTableDataCell scope="row">
+                        {review.empDepartmentName}
+                      </CTableDataCell>
+                      <CTableDataCell scope="row">
+                        {review.empDesignationName}
+                      </CTableDataCell>
+                      <CTableDataCell scope="row">
+                        {review.cycleStartDate}
+                      </CTableDataCell>
+                      <CTableDataCell scope="row">
+                        {review.empAvgRating}
+                      </CTableDataCell>
+                      <CTableDataCell scope="row">
+                        {review.overallAvgRating || 'N/A'}
+                      </CTableDataCell>
+                      <CTableDataCell scope="row">
+                        <CBadge color="success"> {review.formStatus}</CBadge>
+                      </CTableDataCell>
+                    </CTableRow>
+                  )
+                })}
+              </CTableBody>
+            </CTable>
+            &nbsp;&nbsp;
+            <CRow>
+              <CCol xs={4}>
+                <p>
+                  <strong>Total Records: {getReviewDetails.length}</strong>
+                </p>
+              </CCol>
+              <CCol xs={3}>
+                {getReviewDetails.length > 20 && (
+                  <OPageSizeSelect
+                    handlePageSizeSelectChange={handlePageSizeSelectChange}
+                  />
+                )}
+              </CCol>
+              {getReviewDetails.length > 20 && (
+                <CCol
+                  xs={5}
+                  className="d-grid gap-2 d-md-flex justify-content-md-end"
+                >
+                  <OPagination
+                    currentPage={currentPage}
+                    pageSetter={setCurrentPage}
+                    paginationRange={paginationRange}
+                  />
+                </CCol>
+              )}
+            </CRow>
+          </>
+        ) : (
+          <>
+            <CTable striped>
+              <CTableHead>
+                <CTableRow>
+                  <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">ID</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Employee Name</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Manager Name</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Department</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Designation</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Month</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">
+                    Emp Avg Rating
+                  </CTableHeaderCell>
+                  <CTableHeaderCell scope="col">
+                    Managers Avg Rating
+                  </CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Status</CTableHeaderCell>
+                </CTableRow>
+              </CTableHead>
+            </CTable>
+            &nbsp;&nbsp;
+            <CCol xs={4}>
+              <p>
+                <strong>No Records Found... </strong>
+              </p>
+            </CCol>
+          </>
+        )}
       </CCardBody>
-      <br></br>
-      <strong>
-        {getReviewDetails?.length
-          ? `Total Records: ${getReviewDetails?.length}`
-          : `No Records Found...`}
-      </strong>
     </>
   )
 }
