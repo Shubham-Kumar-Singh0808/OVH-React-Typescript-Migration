ff915f00e06dfeacecc1c61767f4021c6526fe84
Vinesh Merugu <vineshmerugu1.com>
2022-06-15 12:24:01 +0530

    Added Tabs

--- src/pages/MyProfile/ProfileLandingPage/MyProfileTabs.tsx
+++ src/pages/MyProfile/ProfileLandingPage/MyProfileTabs.tsx
@@ -47,8 +47,9 @@ const MyProfileTabs = (): JSX.Element => {
         2: <BasicInfoTab />,
         3: <PersonalInfoTab handleActiveTab={handleActiveTab} />,
         4: <QualificationDetails />,
-        5: <h1>Review</h1>,
-        6: <EmployeeMyAssets />,
+        5: <h1>Reviews</h1>,
+        6: <h1>Projects</h1>,
+        8: <EmployeeMyAssets />,
         9: <EmployeeProfileHistory />,
       }
       return showTabContent[tabKey] || 'Tab Content not available'
