233b48934372e9caca6b5705031ce07864c3a418
kem_harold <kemharold.garate@raybiztech.com>
2022-06-20 17:34:19 +0800

    added ckEditorUtils

--- src/pages/MyProfile/QualificationsTab/EmployeeCertifications/AddUpdateEmployeeCertification.tsx
+++ src/pages/MyProfile/QualificationsTab/EmployeeCertifications/AddUpdateEmployeeCertification.tsx
@@ -20,6 +20,7 @@ import { CKEditor, CKEditorEventHandler } from 'ckeditor4-react'
 import OToast from '../../../../components/ReusableComponent/OToast'
 import moment from 'moment'
 import { reduxServices } from '../../../../reducers/reduxServices'
+import { ckeditorConfig } from '../../../../utils/ckEditorUtils'
 
 function AddUpdateEmployeeCertification({
   isEditCertificationDetails = false,
@@ -303,37 +304,6 @@ function AddUpdateEmployeeCertification({
     }
   }
 
-  const config = {
-    toolbarGroups: [
-      { name: 'styles', groups: ['styles'] },
-      { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
-      { name: 'clipboard', groups: ['clipboard', 'undo'] },
-      {
-        name: 'paragraph',
-        groups: ['blocks', 'list', 'indent', 'align', 'bidi', 'paragraph'],
-      },
-      {
-        name: 'editing',
-        groups: ['find', 'selection', 'spellchecker', 'editing'],
-      },
-      { name: 'links', groups: ['links'] },
-      { name: 'insert', groups: ['insert'] },
-      { name: 'forms', groups: ['forms'] },
-      { name: 'tools', groups: ['tools'] },
-      { name: 'document', groups: ['mode', 'document', 'doctools'] },
-      { name: 'others', groups: ['others'] },
-      { name: 'colors', groups: ['colors'] },
-      { name: 'about', groups: ['about'] },
-    ],
-    format_tags: 'p;h1;h2;h3;h4;h5;h6;pre',
-    extraPlugins: 'justify, uploadimage',
-    filebrowserUploadMethod: 'form',
-    filebrowserUploadUrl: '/uploader/upload',
-    removeButtons:
-      'Subscript,Superscript,Cut,Paste,Copy,PasteText,PasteFromWord,Scayt,Anchor,HorizontalRule,SpecialChar,Maximize,Source,Strike,Styles,About,Indent,Outdent,Blockquote',
-    removeDialogTabs: 'image:Link;image:Upload;image:Advanced',
-  }
-
   const handleDescription = (description: string) => {
     setAddCertification((prevState) => {
       return { ...prevState, ...{ description: description } }
@@ -604,7 +574,7 @@ function AddUpdateEmployeeCertification({
                   onChange: CKEditorEventHandler<'change'>
                 }>
                   initData={addCertification?.description}
-                  config={config}
+                  config={ckeditorConfig}
                   debug={true}
                   onChange={({ editor }) => {
                     handleDescription(editor.getData().trim())
