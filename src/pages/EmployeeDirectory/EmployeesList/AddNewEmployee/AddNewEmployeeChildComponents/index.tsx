9ba5a6b79fe95424c5291773588f03190597376e
ocabafox <ocabafox@gmail.com>
2022-06-23 18:44:26 +0800

    remove unused file from add new employee chield component

--- src/pages/EmployeeDirectory/EmployeesList/AddNewEmployee/AddNewEmployeeChildComponents/index.tsx
+++ src/pages/EmployeeDirectory/EmployeesList/AddNewEmployee/AddNewEmployeeChildComponents/index.tsx
@@ -1,40 +1,26 @@
 import Birthday from './Birthday'
-import Country from './Country'
-import Department from './Departments'
 import Designation from './Designation'
 import EmploymentContract from './EmploymentContract'
-import EmploymentType from './EmploymentType'
 import Experience from './Experience'
 import FullName from './FullName'
-import Gender from './Gender'
 import HRAssociate from './HRAssociate'
-import JobType from './JobType'
 import JoinedDate from './JoinedDate'
 import ProjectManager from './ProjectManager'
 import ReportingManager from './ReportingManager'
-import Role from './Role'
 import Shift from './Shift'
-import Technology from './Technology'
 import UserNameEmail from './UserNameEmail'
 import WorkFrom from './WorkFrom'
 
 export {
-  Department,
   FullName,
   UserNameEmail,
-  Gender,
   Birthday,
   JoinedDate,
   Experience,
-  Technology,
   Designation,
-  Role,
   ReportingManager,
   HRAssociate,
   ProjectManager,
-  EmploymentType,
-  JobType,
-  Country,
   Shift,
   EmploymentContract,
   WorkFrom,
