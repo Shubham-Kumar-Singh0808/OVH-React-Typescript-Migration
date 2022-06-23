0c978f19cf53080cc3204b66064165088fc1cbcc
ocabafox <ocabafox@gmail.com>
2022-06-23 19:11:30 +0800

    remove fragment

--- src/pages/EmployeeDirectory/EmployeesList/AddNewEmployee/AddNewEmployeeChildComponents/Birthday.tsx
+++ src/pages/EmployeeDirectory/EmployeesList/AddNewEmployee/AddNewEmployeeChildComponents/Birthday.tsx
@@ -12,34 +12,32 @@ const Birthday = ({
 }: DateChangeHandlerProp): JSX.Element => {
   const date = dateValue == null ? '' : dateValue.toLocaleDateString()
   return (
-    <>
-      <CRow className="mb-3">
-        <CFormLabel
-          {...dynamicFormLabelProps(
-            'birthday',
-            'col-sm-3 col-form-label text-end',
-          )}
-        >
-          Birthday:
-          <span className={showIsRequired(date)}>*</span>
-        </CFormLabel>
-        <CCol sm={3}>
-          <DatePicker
-            id="birthday"
-            className="form-control form-control-sm sh-date-picker"
-            maxDate={new Date()}
-            peekNextMonth
-            showMonthDropdown
-            showYearDropdown
-            dropdownMode="select"
-            placeholderText="Select birth date"
-            name="birthday"
-            value={date}
-            onChange={(date: Date) => onDateChangeHandler(date)}
-          />
-        </CCol>
-      </CRow>
-    </>
+    <CRow className="mb-3">
+      <CFormLabel
+        {...dynamicFormLabelProps(
+          'birthday',
+          'col-sm-3 col-form-label text-end',
+        )}
+      >
+        Birthday:
+        <span className={showIsRequired(date)}>*</span>
+      </CFormLabel>
+      <CCol sm={3}>
+        <DatePicker
+          id="birthday"
+          className="form-control form-control-sm sh-date-picker"
+          maxDate={new Date()}
+          peekNextMonth
+          showMonthDropdown
+          showYearDropdown
+          dropdownMode="select"
+          placeholderText="Select birth date"
+          name="birthday"
+          value={date}
+          onChange={(date: Date) => onDateChangeHandler(date)}
+        />
+      </CCol>
+    </CRow>
   )
 }
 
