f47c2b45cad2a9f0571fe5ec77ab017f47e272c9
Jesury <jesurythomas@gmail.com>
2022-06-24 13:38:39 +0800

    Fixed some issues

--- src/reducers/MyProfile/ProjectsTab/employeeProjectSlice.test.tsx
+++ src/reducers/MyProfile/ProjectsTab/employeeProjectSlice.test.tsx
@@ -1,6 +1,6 @@
 import employeeProjectSlice from './employeeProjectSlice'
 
-fdescribe('Employee Projects Reducer Test', () => {
+describe('Employee Projects Reducer Test', () => {
   it('should return the initial state when passed an empty action', () => {
     const initialState = undefined
     const action = { type: '' }
