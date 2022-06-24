f9c3dcf4ed1872eddd509e5751fa4f109c16ec2d
Jesury <jesurythomas@gmail.com>
2022-06-24 13:27:22 +0800

    Fixed some issues

--- src/pages/MyProfile/ProjectsTab/EmployeeProjectsTable.tsx
+++ src/pages/MyProfile/ProjectsTab/EmployeeProjectsTable.tsx
@@ -69,6 +69,11 @@ const EmployeeProjectsTable = (): JSX.Element => {
           </CTableBody>
         </CTable>
       </CCardBody>
+      <p>
+        <strong>
+          {employeeProjects && `Total Records: ${employeeProjects.Projsize}`}
+        </strong>
+      </p>
     </>
   )
 }
