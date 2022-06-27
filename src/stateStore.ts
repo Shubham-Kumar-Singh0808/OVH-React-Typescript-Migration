defa33216ced04aa767a4f6a8357b097bb3684ad
kem_harold <kemharold.garate@raybiztech.com>
2022-06-27 18:55:57 +0800

    employee report

--- src/stateStore.ts
+++ src/stateStore.ts
@@ -24,7 +24,7 @@ import sidebarMenuSliceReducer from './reducers/SidebarMenu/sidebarMenuSlice'
 import thunkMiddleware from 'redux-thunk'
 import skillReducer from './reducers/MyProfile/Skills/skillSlice'
 import userRolesAndPermissionsReducer from './reducers/Settings/UserRolesConfiguration/userRolesAndPermissionsSlice'
-import employeeReportReducer from './reducers/EmployeeDirectory/EmployeeReport/employeeReportSlice'
+import employeeReportReducer from './reducers/EmployeeDirectory/EmployeeReport/'
 import employeeDesignationReportReducer from './reducers/EmployeeDirectory/EmployeeReport/EmployeeDesignationReport/employeeDesignationReportSlice'
 import visaListReducer from './reducers/EmployeeDirectory/VisaList/visaListSlice'
 
