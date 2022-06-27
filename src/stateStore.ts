cf39bb21bab36b943e3ea18eb0a88dff61231441
kem_harold <kemharold.garate@raybiztech.com>
2022-06-27 14:39:01 +0800

    update store

--- src/stateStore.ts
+++ src/stateStore.ts
@@ -22,11 +22,11 @@ import qualificationCategoryReducer from './reducers/MyProfile/QualificationsTab
 import shiftConfigurationReducer from './reducers/EmployeeDirectory/EmployeesList/AddNewEmployee/ShiftConfiguration/shiftConfigurationSlice'
 import sidebarMenuSliceReducer from './reducers/SidebarMenu/sidebarMenuSlice'
 import skillReducer from './reducers/MyProfile/Skills/skillSlice'
-import thunkMiddleware from 'redux-thunk'
 import userRolesAndPermissionsReducer from './reducers/Settings/UserRolesConfiguration/userRolesAndPermissionsSlice'
 import employeeReportReducer from './reducers/EmployeeDirectory/EmployeeReport/employeeReportSlice'
 import employeeDesignationReportReducer from './reducers/EmployeeDirectory/EmployeeReport/EmployeeDesignationReport/employeeDesignationReportSlice'
 import visaListReducer from './reducers/EmployeeDirectory/VisaList/visaListSlice'
+import thunkMiddleware from 'redux-thunk'
 
 export const allReducers = {
   app: appReducer,
