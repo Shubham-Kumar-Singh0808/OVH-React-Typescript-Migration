b755ce9e385e4c6a7daa25404d795f460d26bd82
saikiran banothu <saikiran.banothu@raybiztech.com>
2022-07-04 15:45:43 +0530

    rebase

--- src/reducers/reduxServices.ts
+++ src/reducers/reduxServices.ts
@@ -59,3 +59,4 @@ export const reduxServices = {
   employeeHandbookSettings: employeeHandbookSettingService,
   employeeLeaveSettings: employeeLeaveSettingsService,
   technology: technologyService,
+}
