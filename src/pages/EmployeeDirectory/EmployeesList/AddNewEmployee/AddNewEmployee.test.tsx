21e6107e1f224d241b2ebd08d84e51dd56cd53d3
ocabafox <ocabafox@gmail.com>
2022-07-04 18:49:05 +0800

    add test

--- src/pages/EmployeeDirectory/EmployeesList/AddNewEmployee/AddNewEmployee.test.tsx
+++ src/pages/EmployeeDirectory/EmployeesList/AddNewEmployee/AddNewEmployee.test.tsx
@@ -5,6 +5,7 @@ import { render, screen } from '@testing-library/react'
 import { EnhancedStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import React from 'react'
+import userEvent from '@testing-library/user-event'
 import AddNewEmployee from '.'
 import stateStore from '../../../../stateStore'
 
@@ -16,11 +17,16 @@ const ReduxProvider = ({
   reduxStore: EnhancedStore
 }) => <Provider store={reduxStore}>{children}</Provider>
 
+const expectComponentToBeRendered = () => {
+  expect(screen.getByRole('button')).toBeInTheDocument()
+  expect(screen.getByRole('button')).toBeDisabled()
+}
+
 describe('Add New Employee Testing', () => {
   test('should render add new employee form without crashing', () => {
     render(
       <ReduxProvider reduxStore={stateStore}>
-        <AddNewEmployee setToggleShift={jest.fn()} />
+        <AddNewEmployee />
       </ReduxProvider>,
     )
     screen.debug()
@@ -29,7 +35,7 @@ describe('Add New Employee Testing', () => {
   test('should be able to render Add Family button', () => {
     render(
       <ReduxProvider reduxStore={stateStore}>
-        <AddNewEmployee setToggleShift={jest.fn()} />
+        <AddNewEmployee />
       </ReduxProvider>,
     )
     expect(screen.getByTestId('add-new-employee')).toBeInTheDocument()
@@ -38,7 +44,7 @@ describe('Add New Employee Testing', () => {
   test('should be able to render Clear button', () => {
     render(
       <ReduxProvider reduxStore={stateStore}>
-        <AddNewEmployee setToggleShift={jest.fn()} />
+        <AddNewEmployee />
       </ReduxProvider>,
     )
     expect(screen.getByTestId('clear-new-employee')).toBeInTheDocument()
@@ -47,10 +53,33 @@ describe('Add New Employee Testing', () => {
   test('should render 2 input components', () => {
     render(
       <ReduxProvider reduxStore={stateStore}>
-        <AddNewEmployee setToggleShift={jest.fn()} />
+        <AddNewEmployee />
       </ReduxProvider>,
     )
     expect(screen.getByPlaceholderText('User Name')).toBeInTheDocument()
     expect(screen.getByPlaceholderText('Email')).toBeInTheDocument()
+  })
+
+  test('should stay disable add button when input is empty', () => {
+    render(
+      <ReduxProvider reduxStore={stateStore}>
+        <AddNewEmployee />
+      </ReduxProvider>,
+    )
+    expectComponentToBeRendered()
+    expect(screen.getByTestId('add-new-employee')).toBeDisabled()
+  })
+
+  test('should enable clear button when input is not empty', () => {
+    render(
+      <ReduxProvider reduxStore={stateStore}>
+        <AddNewEmployee />
+      </ReduxProvider>,
+    )
+
+    expectComponentToBeRendered()
+
+    userEvent.type(screen.getByRole('textbox'), 'test input..')
+    expect(screen.getByTestId('clear-new-employee')).not.toBeDisabled()
   })
 })
