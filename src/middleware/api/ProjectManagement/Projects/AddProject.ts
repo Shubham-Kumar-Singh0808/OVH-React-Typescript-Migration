d72432c5eb6c59c7ee79424ded6dd032730a0581
ocabafox <ocabafox@gmail.com>
2022-08-09 00:33:59 +0800

    add test for autoselect component

--- src/middleware/api/ProjectManagement/Projects/AddProject.ts
+++ src/middleware/api/ProjectManagement/Projects/AddProject.ts
@@ -3,7 +3,10 @@ import {
   getAuthenticatedRequestConfig,
   useAxios,
 } from '../../../../utils/apiUtils'
-import { ProjectClients } from '../../../../types/ProjectManagement/Project/AddProject/AddProjectTypes'
+import {
+  ProjectClients,
+  ProjectDetail,
+} from '../../../../types/ProjectManagement/Project/AddProject/AddProjectTypes'
 
 const getProjectClients = async (): Promise<ProjectClients[]> => {
   const requestConfig = getAuthenticatedRequestConfig({
@@ -15,7 +18,20 @@ const getProjectClients = async (): Promise<ProjectClients[]> => {
   return response.data
 }
 
+const addProject = async (
+  addProjectDetails: ProjectDetail,
+): Promise<ProjectDetail> => {
+  const requestConfig = getAuthenticatedRequestConfig({
+    url: employeeProjectsApiConfig.addProject,
+    method: AllowedHttpMethods.post,
+    data: addProjectDetails,
+  })
+  const response = await useAxios(requestConfig)
+  return response.data
+}
+
 const AddProject = {
+  addProject,
   getProjectClients,
 }
 
