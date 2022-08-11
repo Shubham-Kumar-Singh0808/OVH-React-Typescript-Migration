b6989c693615bc40bc90a497fe6e229487585556
Vinesh Merugu <vineshmerugu1.com>
2022-08-11 15:09:46 +0530

    Resolved Issues

--- src/pages/Leaves/LeaveReports/LeaveReportFilterOption.test.tsx
+++ src/pages/Leaves/LeaveReports/LeaveReportFilterOption.test.tsx
@@ -3,12 +3,18 @@ import React from 'react'
 import userEvent from '@testing-library/user-event'
 import LeaveReportFilterOption from './LeaveReportFilterOption'
 import { render, screen } from '../../../test/testUtils'
-import { mockCreditYearData } from '../../../test/data/LeaveReportData'
+
+const mockSetSelectYear = jest.fn()
 
 describe('LeaveReportFilter Options Component Testing', () => {
   describe('Filter Options component without value', () => {
     beforeEach(() => {
-      render(<LeaveReportFilterOption />)
+      render(
+        <LeaveReportFilterOption
+          selectYear={''}
+          setSelectYear={mockSetSelectYear}
+        />,
+      )
     })
 
     test('should render labels', () => {
@@ -30,21 +36,5 @@ describe('LeaveReportFilter Options Component Testing', () => {
       userEvent.type(screen.getByPlaceholderText('Search Employees'), 'Java')
       expect(screen.getByTestId('search-btn1')).toBeEnabled()
     })
-  })
-})
-describe('Select Year Field', () => {
-  test('should render Select year field without crashing', () => {
-    render(<LeaveReportFilterOption />, {
-      preloadedState: {
-        leaveReport: {
-          selectFinancialYear: mockCreditYearData,
-        },
-      },
-    })
-
-    const countryDropdown = screen.getByTestId('form-select2')
-    userEvent.selectOptions(countryDropdown, ['2019'])
-    expect(mockCreditYearData).toBeCalledWith('1')
-    expect(countryDropdown).toBeInTheDocument()
   })
 })
