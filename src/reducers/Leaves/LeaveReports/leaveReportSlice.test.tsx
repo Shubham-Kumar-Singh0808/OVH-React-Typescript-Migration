cee7eb8fa76e0c5b5e937a61dccfb8315a580d9e
Vinesh Merugu <vineshmerugu1.com>
2022-08-25 12:20:31 +0530

    unit test cases

--- src/reducers/Leaves/LeaveReports/leaveReportSlice.test.tsx
+++ src/reducers/Leaves/LeaveReports/leaveReportSlice.test.tsx
@@ -1,18 +1,83 @@
-import leaveReportSlice from './leaveReportSlice'
+import reducer, { leaveReportService } from './leaveReportSlice'
 import { ApiLoadingState } from '../../../middleware/api/apiList'
+import { LeaveReportState } from '../../../types/Leaves/LeaveReports/leaveReportTypes'
+import {
+  mockCreditYearData,
+  mockLeaveReportData,
+} from '../../../test/data/LeaveReportData'
 
-describe('Employee leave Report Reducer Test', () => {
-  it('should return the initial state when passed an empty action', () => {
-    const initialState = undefined
-    const action = { type: '' }
-    const result = leaveReportSlice(initialState, action)
-    expect(result).toEqual({
+describe('LeaveReport Slice', () => {
+  describe('Reducer', () => {
+    const initialLeaveReportState = {
       leaveSummaries: { list: [], size: 0, name: '', length: 0 },
       selectFinancialYear: [],
-      error: null,
       isLoading: ApiLoadingState.idle,
+      error: null,
       financialYear: 0,
       listSize: 0,
+    } as LeaveReportState
+
+    it('Should be able to set isLoading to "loading" if getAllEmployeesLeaveSummaries is pending', () => {
+      const action = {
+        type: leaveReportService.getAllEmployeesLeaveSummaries.pending.type,
+      }
+      const state = reducer(initialLeaveReportState, action)
+      expect(state).toEqual({
+        leaveSummaries: { list: [], size: 0, name: '', length: 0 },
+        selectFinancialYear: [],
+        isLoading: ApiLoadingState.loading,
+        error: null,
+        financialYear: 0,
+        listSize: 0,
+      })
+    })
+
+    it('Should be able to set isLoading to "success" if getAllEmployeesLeaveSummaries is fulfilled', () => {
+      const action = {
+        type: leaveReportService.getAllEmployeesLeaveSummaries.fulfilled.type,
+        payload: mockLeaveReportData,
+      }
+      const state = reducer(initialLeaveReportState, action)
+      expect(state).toEqual({
+        leaveSummaries: mockLeaveReportData,
+        selectFinancialYear: [],
+        isLoading: ApiLoadingState.succeeded,
+        error: null,
+        financialYear: 0,
+        listSize: 217,
+      })
+    })
+
+    it('Should be able to set isLoading to "success" if creditedYearDetails is fulfilled', () => {
+      const action = {
+        type: leaveReportService.creditedYearDetails.fulfilled.type,
+        payload: mockCreditYearData,
+      }
+      const state = reducer(initialLeaveReportState, action)
+      expect(state).toEqual({
+        leaveSummaries: { list: [], size: 0, name: '', length: 0 },
+        selectFinancialYear: mockCreditYearData,
+        isLoading: ApiLoadingState.succeeded,
+        error: null,
+        financialYear: 0,
+        listSize: 0,
+      })
+    })
+
+    it('Should be able to set isLoading to "success" if searchLeaveSummaries is fulfilled', () => {
+      const action = {
+        type: leaveReportService.searchLeaveSummaries.fulfilled.type,
+        payload: mockLeaveReportData,
+      }
+      const state = reducer(initialLeaveReportState, action)
+      expect(state).toEqual({
+        leaveSummaries: mockLeaveReportData,
+        selectFinancialYear: [],
+        isLoading: ApiLoadingState.succeeded,
+        error: null,
+        financialYear: 0,
+        listSize: 0,
+      })
     })
   })
 })
