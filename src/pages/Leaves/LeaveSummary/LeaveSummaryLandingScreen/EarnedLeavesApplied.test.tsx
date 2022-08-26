9c4208691ed35cb7ce251f530d8e2edcb5e62586
saikirannaik <saikirannayak222@gmail.com>
2022-08-26 14:49:05 +0530

    updated EarnedLeavesApplied.test.tsx

--- src/pages/Leaves/LeaveSummary/LeaveSummaryLandingScreen/EarnedLeavesApplied.test.tsx
+++ src/pages/Leaves/LeaveSummary/LeaveSummaryLandingScreen/EarnedLeavesApplied.test.tsx
@@ -2,10 +2,17 @@ import '@testing-library/jest-dom'
 import React from 'react'
 import EarnedLeavesApplied from './EarnedLeavesApplied'
 import { render, screen } from '../../../../test/testUtils'
+import { mockLeaveSummary } from '../../../../test/data/leaveSummaryData'
 
 describe('Leave Summary Component Testing', () => {
   test('should render Leave Summary Component without crashing', () => {
-    render(<EarnedLeavesApplied />)
+    render(<EarnedLeavesApplied />, {
+      preloadedState: {
+        employeeLeaveSummary: {
+          employeeLeaveSummary: mockLeaveSummary,
+        },
+      },
+    })
     expect(screen.getByText('Casual')).toBeInTheDocument()
     expect(screen.getByText('PAID')).toBeInTheDocument()
   })
