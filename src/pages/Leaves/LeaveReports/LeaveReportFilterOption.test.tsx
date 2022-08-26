cde5ce10dc2a4e10541f936d7f239c80ccc7dbf9
Vinesh Merugu <vineshmerugu1.com>
2022-08-26 17:10:04 +0530

    unit test cases

--- src/pages/Leaves/LeaveReports/LeaveReportFilterOption.test.tsx
+++ src/pages/Leaves/LeaveReports/LeaveReportFilterOption.test.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import userEvent from '@testing-library/user-event'
 import LeaveReportFilterOption from './LeaveReportFilterOption'
 import { render, screen } from '../../../test/testUtils'
-import { mockCreditYearData } from '../../../test/data/LeaveReportData'
 
 const mockSetSelectYear = jest.fn()
 
@@ -36,28 +35,6 @@ describe('LeaveReportFilter Options Component Testing', () => {
       expect(screen.getByTestId('search-btn1')).not.toBeEnabled()
       userEvent.type(screen.getByPlaceholderText('Search Employees'), 'Java')
       expect(screen.getByTestId('search-btn1')).toBeEnabled()
-    })
-  })
-  describe('LeaveType component with data', () => {
-    beforeEach(() => {
-      render(
-        <LeaveReportFilterOption
-          selectYear={''}
-          setSelectYear={mockSetSelectYear}
-        />,
-        {
-          preloadedState: {
-            leaveReport: {
-              selectFinancialYear: mockCreditYearData,
-            },
-          },
-        },
-      )
-    })
-    test('should select dropdown value', () => {
-      const LeaveReportSelector = screen.getByTestId('form-select2')
-      userEvent?.selectOptions(LeaveReportSelector, ['2022'])
-      expect(LeaveReportSelector).toHaveValue('2022')
     })
   })
 })
