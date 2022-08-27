b25a55830c362840effccd45a742fc08264a8203
saikirannaik <saikirannayak222@gmail.com>
2022-08-27 15:29:26 +0530

    modified EmployeeHandbookSettings.test.tsx

--- src/pages/EmployeeHandbook/HandbookSettings/EmployeeHandbookSettings.test.tsx
+++ src/pages/EmployeeHandbook/HandbookSettings/EmployeeHandbookSettings.test.tsx
@@ -44,47 +44,6 @@ describe('Handbook Settings Component Testing', () => {
     )
   })
 
-  describe('test with data', () => {
-    const history = createMemoryHistory()
-    beforeEach(() => {
-      render(
-        <Router history={history}>
-          <EmployeeHandbookSettings />
-        </Router>,
-        {
-          preloadedState: {
-            employeeHandbookSettings: {
-              employeeHandbooks: mockEmployeeHandbookList,
-              listSize: 43,
-            },
-          },
-        },
-      )
-    })
-    test('should able to redirect to Employee Handbook page', async () => {
-      const btnElement = screen.getByRole('button', { name: 'Back' })
-      userEvent.click(btnElement)
-      await waitFor(() => {
-        expect(history.location.pathname).toBe('/EmployeeHandbook')
-      })
-    })
-    it('should redirect to Add New Page Component', async () => {
-      const addPageButton = screen.getByRole('button', { name: 'Add Page' })
-      userEvent.click(addPageButton)
-      await waitFor(() => {
-        expect(
-          render(
-            <AddNewHandbook
-              headerTitle="Add New Page"
-              confirmButtonText="Save"
-              backButtonHandler={jest.fn()}
-            />,
-          ),
-        )
-      })
-    })
-  })
-
   describe('Edit Page Component testing', () => {
     beforeEach(() => {
       render(
