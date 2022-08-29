085a1847bdbdf7cf44baafc55909197c90f768f1
Vinesh Merugu <vineshmerugu1.com>
2022-08-29 16:21:58 +0530

    unit test cases

---
+++ src/reducers/Support/Report/ticketReportSlice.test.tsx
@@ -0,0 +1,97 @@
+import reducer, { ticketReportService } from './ticketReportSlice'
+import { ApiLoadingState } from '../../../middleware/api/apiList'
+import { TicketReportSliceState } from '../../../types/Support/Report/ticketReportTypes'
+import {
+  mockTicketDetailsData,
+  mockTicketReportData,
+} from '../../../test/data/ticketReportsData'
+
+describe('Ticket Report Slice', () => {
+  describe('Reducer', () => {
+    const initialTicketReportState = {
+      currentPage: 1,
+      pageSize: 20,
+      ticketsReportList: [],
+      getTicketsReport: { list: [], size: 0 },
+      departmentCategoryList: [],
+      departmentNameList: [],
+      isLoading: ApiLoadingState.idle,
+      ticketsDetailsList: [],
+      getTicketDetails: { list: [], size: 0 },
+    } as TicketReportSliceState
+
+    it('Should be able to set isLoading to "loading" if getTicketsReport is pending', () => {
+      const action = {
+        type: ticketReportService.getTicketsReport.pending.type,
+      }
+      const state = reducer(initialTicketReportState, action)
+      expect(state).toEqual({
+        currentPage: 1,
+        pageSize: 20,
+        ticketsReportList: [],
+        getTicketsReport: { list: [], size: 0 },
+        departmentCategoryList: [],
+        departmentNameList: [],
+        isLoading: ApiLoadingState.loading,
+        ticketsDetailsList: [],
+        getTicketDetails: { list: [], size: 0 },
+      })
+    })
+
+    it('Should be able to set isLoading to "loading" if getTicketDetails is pending', () => {
+      const action = {
+        type: ticketReportService.getTicketDetails.pending.type,
+      }
+      const state = reducer(initialTicketReportState, action)
+      expect(state).toEqual({
+        currentPage: 1,
+        pageSize: 20,
+        ticketsReportList: [],
+        getTicketsReport: { list: [], size: 0 },
+        departmentCategoryList: [],
+        departmentNameList: [],
+        isLoading: ApiLoadingState.loading,
+        ticketsDetailsList: [],
+        getTicketDetails: { list: [], size: 0 },
+      })
+    })
+
+    it('Should be able to set isLoading to "success" if getTicketsReport is fulfilled', () => {
+      const action = {
+        type: ticketReportService.getTicketsReport.fulfilled.type,
+        payload: mockTicketReportData,
+      }
+      const state = reducer(initialTicketReportState, action)
+      expect(state).toEqual({
+        currentPage: 1,
+        pageSize: 20,
+        ticketsReportList: undefined,
+        getTicketsReport: mockTicketReportData,
+        departmentCategoryList: [],
+        departmentNameList: [],
+        isLoading: ApiLoadingState.succeeded,
+        ticketsDetailsList: [],
+        getTicketDetails: { list: [], size: 0 },
+      })
+    })
+
+    it('Should be able to set isLoading to "success" if getTicketDetails is fulfilled', () => {
+      const action = {
+        type: ticketReportService.getTicketDetails.fulfilled.type,
+        payload: mockTicketDetailsData,
+      }
+      const state = reducer(initialTicketReportState, action)
+      expect(state).toEqual({
+        currentPage: 1,
+        pageSize: 20,
+        ticketsReportList: [],
+        getTicketsReport: { list: [], size: 0 },
+        departmentCategoryList: [],
+        departmentNameList: [],
+        isLoading: ApiLoadingState.succeeded,
+        ticketsDetailsList: undefined,
+        getTicketDetails: mockTicketDetailsData,
+      })
+    })
+  })
+})
