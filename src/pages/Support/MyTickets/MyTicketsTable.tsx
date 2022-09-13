4ebf50612e62604b5e42aea62b5e407ebfaa1190
Vinesh Merugu <vineshmerugu1.com>
2022-09-13 19:03:28 +0530

    Resolved Issues

--- src/pages/Support/MyTickets/MyTicketsTable.tsx
+++ src/pages/Support/MyTickets/MyTicketsTable.tsx
@@ -16,15 +16,23 @@ import OModal from '../../../components/ReusableComponent/OModal'
 import OPageSizeSelect from '../../../components/ReusableComponent/OPageSizeSelect'
 import OPagination from '../../../components/ReusableComponent/OPagination'
 import OToast from '../../../components/ReusableComponent/OToast'
-import { usePagination } from '../../../middleware/hooks/usePagination'
 import { reduxServices } from '../../../reducers/reduxServices'
 import { useAppDispatch, useTypedSelector } from '../../../stateStore'
-import { currentPageData } from '../../../utils/paginationUtils'
 
 const MyTicketsTable = ({
   setToggle,
+  paginationRange,
+  currentPage,
+  setCurrentPage,
+  pageSize,
+  setPageSize,
 }: {
   setToggle: (value: string) => void
+  paginationRange: number[]
+  currentPage: number
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>
+  pageSize: number
+  setPageSize: React.Dispatch<React.SetStateAction<number>>
 }): JSX.Element => {
   const [isModalVisible, setIsModalVisible] = useState(false)
   const [isCancelModalVisible, setIsCancelModalVisible] = useState(false)
@@ -35,12 +43,8 @@ const MyTicketsTable = ({
     reduxServices.tickets.selectors.allTickets,
   )
 
-  const pageFromState = useTypedSelector(
-    reduxServices.tickets.selectors.pageFromState,
-  )
-
-  const pageSizeFromState = useTypedSelector(
-    reduxServices.tickets.selectors.pageSizeFromState,
+  const listSize = useTypedSelector(
+    reduxServices.tickets.selectors.allTicketsListSize,
   )
 
   const handleModal = (subject: string) => {
@@ -48,25 +52,12 @@ const MyTicketsTable = ({
     setTicketSubject(subject)
   }
 
-  const {
-    paginationRange,
-    setPageSize,
-    setCurrentPage,
-    currentPage,
-    pageSize,
-  } = usePagination(getAllTickets.length, pageSizeFromState, pageFromState)
-
   const handlePageSizeSelectChange = (
     event: React.ChangeEvent<HTMLSelectElement>,
   ) => {
     setPageSize(Number(event.target.value))
     setCurrentPage(1)
   }
-
-  const currentPageItems = useMemo(
-    () => currentPageData(getAllTickets, currentPage, pageSize),
-    [getAllTickets, currentPage, pageSize],
-  )
 
   const handleTicketHistoryClick = (id: number) => {
     setToggle('ticketHistory')
@@ -113,140 +104,149 @@ const MyTicketsTable = ({
 
   return (
     <>
-      <CTable striped className="mt-3">
-        <CTableHead>
-          <CTableRow>
-            <CTableHeaderCell scope="col">Ticket No</CTableHeaderCell>
-            <CTableHeaderCell scope="col">Subject</CTableHeaderCell>
-            <CTableHeaderCell scope="col">Description</CTableHeaderCell>
-            <CTableHeaderCell scope="col">Priority</CTableHeaderCell>
-            <CTableHeaderCell scope="col">Start Date</CTableHeaderCell>
-            <CTableHeaderCell scope="col">End Date</CTableHeaderCell>
-            <CTableHeaderCell scope="col">Approval Status</CTableHeaderCell>
-            <CTableHeaderCell scope="col">Ticket Status</CTableHeaderCell>
-            <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
-          </CTableRow>
-        </CTableHead>
-        <CTableBody>
-          {currentPageItems &&
-            currentPageItems?.map((ticket, index) => {
-              const ticketSubjectLimit =
-                ticket.subject && ticket.subject.length > 30
-                  ? `${ticket.subject.substring(0, 30)}...`
-                  : ticket.subject
+      {getAllTickets.length ? (
+        <>
+          <CTable striped className="mt-3">
+            <CTableHead>
+              <CTableRow>
+                <CTableHeaderCell scope="col">Ticket No</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Subject</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Description</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Priority</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Start Date</CTableHeaderCell>
+                <CTableHeaderCell scope="col">End Date</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Approval Status</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Ticket Status</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
+            <CTableBody>
+              {getAllTickets &&
+                getAllTickets?.map((ticket, index) => {
+                  const ticketSubjectLimit =
+                    ticket.subject && ticket.subject.length > 30
+                      ? `${ticket.subject.substring(0, 30)}...`
+                      : ticket.subject
 
-              const ticketDiscriptionLimit =
-                ticket.description && ticket.description.length > 30
-                  ? `${ticket.description.substring(0, 30)}...`
-                  : ticket.description
+                  const ticketDiscriptionLimit =
+                    ticket.description && ticket.description.length > 30
+                      ? `${ticket.description.substring(0, 30)}...`
+                      : ticket.description
 
-              const ticketDescription =
-                ticket.description !== null
-                  ? parse(ticketDiscriptionLimit)
-                  : 'N/A'
-              return (
-                <CTableRow key={index}>
-                  <CTableDataCell>{ticket.id}</CTableDataCell>
-                  {ticketSubjectLimit ? (
-                    <CTableDataCell>
-                      <CLink
-                        className="cursor-pointer text-decoration-none text-primary"
-                        data-testid={`emp-subject${index}`}
-                        onClick={() => handleModal(ticket.subject)}
-                      >
-                        {parse(ticketSubjectLimit)}
-                      </CLink>
-                    </CTableDataCell>
-                  ) : (
-                    <CTableDataCell>{`N/A`}</CTableDataCell>
-                  )}
-                  {ticketDiscriptionLimit ? (
-                    <CTableDataCell>
-                      <CLink
-                        className="cursor-pointer text-decoration-none text-primary"
-                        data-testid={`mgr-comments${index}`}
-                        onClick={() => handleModal(ticket.description)}
-                      >
-                        {ticketDescription}
-                      </CLink>
-                    </CTableDataCell>
-                  ) : (
-                    <CTableDataCell>{`N/A`}</CTableDataCell>
-                  )}
-                  <CTableDataCell>{ticket.priority}</CTableDataCell>
-                  <CTableDataCell>{ticket.accessStartDate}</CTableDataCell>
-                  <CTableDataCell>{ticket.accessEndDate}</CTableDataCell>
-                  <CTableDataCell>{ticket.approvalStatus}</CTableDataCell>
-                  <CTableDataCell>{ticket.status}</CTableDataCell>
-                  <CTableDataCell>
-                    <CButton
-                      color="info"
-                      className="btn-ovh me-2"
-                      disabled={ticket.approvalStatus === 'Cancelled'}
-                    >
-                      <i
-                        className="fa fa-pencil-square-o"
-                        aria-hidden="true"
-                      ></i>
-                    </CButton>
-                    <CButton
-                      color="btn btn-warning"
-                      className="btn-ovh me-2"
-                      data-testid="cancel-btn"
-                      onClick={() => handleCancelTicketModal(ticket.id)}
-                      disabled={ticket.approvalStatus === 'Cancelled'}
-                    >
-                      <i
-                        className="fa fa-times text-white"
-                        aria-hidden="true"
-                      ></i>
-                    </CButton>
-                    <CButton
-                      color="info"
-                      className="btn-ovh me-2"
-                      data-testid="history-btn"
-                      onClick={() => handleTicketHistoryClick(ticket.id)}
-                    >
-                      <i
-                        className="fa fa-bar-chart text-white"
-                        aria-hidden="true"
-                      ></i>
-                    </CButton>
-                  </CTableDataCell>
-                </CTableRow>
-              )
-            })}
-        </CTableBody>
-      </CTable>
-      <CRow>
-        <CCol xs={4}>
-          <strong>
-            {getAllTickets?.length
-              ? `Total Records: ${getAllTickets.length}`
-              : `No Records Found...`}
-          </strong>
+                  const ticketDescription =
+                    ticket.description !== null
+                      ? parse(ticketDiscriptionLimit)
+                      : 'N/A'
+                  return (
+                    <CTableRow key={index}>
+                      <CTableDataCell>{ticket.id}</CTableDataCell>
+                      {ticketSubjectLimit ? (
+                        <CTableDataCell>
+                          <CLink
+                            className="cursor-pointer text-decoration-none text-primary"
+                            data-testid={`emp-subject${index}`}
+                            onClick={() => handleModal(ticket.subject)}
+                          >
+                            {parse(ticketSubjectLimit)}
+                          </CLink>
+                        </CTableDataCell>
+                      ) : (
+                        <CTableDataCell>{`N/A`}</CTableDataCell>
+                      )}
+                      {ticketDiscriptionLimit ? (
+                        <CTableDataCell>
+                          <CLink
+                            className="cursor-pointer text-decoration-none text-primary"
+                            data-testid={`mgr-comments${index}`}
+                            onClick={() => handleModal(ticket.description)}
+                          >
+                            {ticketDescription}
+                          </CLink>
+                        </CTableDataCell>
+                      ) : (
+                        <CTableDataCell>{`N/A`}</CTableDataCell>
+                      )}
+                      <CTableDataCell>{ticket.priority}</CTableDataCell>
+                      <CTableDataCell>{ticket.accessStartDate}</CTableDataCell>
+                      <CTableDataCell>{ticket.accessEndDate}</CTableDataCell>
+                      <CTableDataCell>{ticket.approvalStatus}</CTableDataCell>
+                      <CTableDataCell>{ticket.status}</CTableDataCell>
+                      <CTableDataCell>
+                        <CButton
+                          color="info"
+                          className="btn-ovh me-2"
+                          disabled={ticket.approvalStatus === 'Cancelled'}
+                        >
+                          <i
+                            className="fa fa-pencil-square-o"
+                            aria-hidden="true"
+                          ></i>
+                        </CButton>
+                        <CButton
+                          color="btn btn-warning"
+                          className="btn-ovh me-2"
+                          data-testid="cancel-btn"
+                          onClick={() => handleCancelTicketModal(ticket.id)}
+                          disabled={ticket.approvalStatus === 'Cancelled'}
+                        >
+                          <i
+                            className="fa fa-times text-white"
+                            aria-hidden="true"
+                          ></i>
+                        </CButton>
+                        <CButton
+                          color="info"
+                          className="btn-ovh me-2"
+                          data-testid="history-btn"
+                          onClick={() => handleTicketHistoryClick(ticket.id)}
+                        >
+                          <i
+                            className="fa fa-bar-chart text-white"
+                            aria-hidden="true"
+                          ></i>
+                        </CButton>
+                      </CTableDataCell>
+                    </CTableRow>
+                  )
+                })}
+            </CTableBody>
+          </CTable>
+          <CRow>
+            <CCol xs={4}>
+              <p>
+                <strong>Total Records: {listSize}</strong>
+              </p>
+            </CCol>
+            <CCol xs={3}>
+              {listSize > 20 && (
+                <OPageSizeSelect
+                  handlePageSizeSelectChange={handlePageSizeSelectChange}
+                  options={[20, 40, 60, 80]}
+                  selectedPageSize={pageSize}
+                />
+              )}
+            </CCol>
+            {listSize > 20 && (
+              <CCol
+                xs={5}
+                className="gap-1 d-grid d-md-flex justify-content-md-end"
+              >
+                <OPagination
+                  currentPage={currentPage}
+                  pageSetter={setCurrentPage}
+                  paginationRange={paginationRange}
+                />
+              </CCol>
+            )}
+          </CRow>
+        </>
+      ) : (
+        <CCol>
+          <CRow className="category-no-data">
+            <h4 className="text-center">No data to display</h4>
+          </CRow>
         </CCol>
-        <CCol xs={3}>
-          {getAllTickets.length > 20 && (
-            <OPageSizeSelect
-              handlePageSizeSelectChange={handlePageSizeSelectChange}
-              selectedPageSize={pageSize}
-            />
-          )}
-        </CCol>
-        {getAllTickets.length > 20 && (
-          <CCol
-            xs={5}
-            className="d-grid gap-1 d-md-flex justify-content-md-end"
-          >
-            <OPagination
-              currentPage={currentPage}
-              pageSetter={setCurrentPage}
-              paginationRange={paginationRange}
-            />
-          </CCol>
-        )}
-      </CRow>
+      )}
       <OModal
         modalSize="lg"
         alignment="center"
