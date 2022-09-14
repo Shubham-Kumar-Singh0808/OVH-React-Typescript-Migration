cb180b57c3bfdf324c7bb33596feb237f15b96c5
Vinesh Merugu <vineshmerugu1.com>
2022-09-14 18:14:24 +0530

    Resolved Issues

--- src/pages/Leaves/LeaveReports/LeaveReportFilterOption.test.tsx
+++ src/pages/Leaves/LeaveReports/LeaveReportFilterOption.test.tsx
@@ -9,55 +9,17 @@ import {
 } from '../../../test/data/LeaveReportData'
 
 const mockSetSelectYear = jest.fn()
-const mockHandleExportTicketApprovalList = jest.fn()
+
 const result = mockCreditYearData
   ?.filter((value) => value.yearOfEra.value <= 2022)
   .map((val2) => val2.yearOfEra.value)
 const uniqueValue = Array.from(new Set(result))
 
-describe('LeaveReportFilter Options Component Testing', () => {
-  describe('Filter Options component without value', () => {
-    beforeEach(() => {
-      render(
-        <LeaveReportFilterOption
-          selectYear={''}
-          setSelectYear={mockSetSelectYear}
-        />,
-      )
-    })
-
-    test('should render labels', () => {
-      expect(screen.getByText('Select Year:')).toBeInTheDocument()
-    })
-    test('should render Select year filter', () => {
-      const selectYearFilter = screen.findByTestId('leave-form-select2')
-      expect(selectYearFilter).toBeTruthy()
-    })
-
-    test('should render search input field', () => {
-      const searchComponent = screen.getByTestId('searchField')
-      expect(searchComponent).toBeTruthy()
-      const searchInput = screen.findByTestId('searchInput')
-      expect(searchInput).toBeTruthy()
-    })
-    test('multi search button should enable only if we enter the value', () => {
-      expect(screen.getByTestId('search-btn1')).not.toBeEnabled()
-      userEvent.type(screen.getByPlaceholderText('Search Employees'), 'Java')
-      expect(screen.getByTestId('search-btn1')).toBeEnabled()
-    })
-    test('should able to click "click to to export" button', () => {
-      const exportBtn = screen.getByRole('button', { name: 'Click to Export' })
-      userEvent.click(exportBtn)
-      expect(mockHandleExportTicketApprovalList).toHaveBeenCalledTimes(0)
-    })
-  })
-})
-
-describe('Ticket Report Filter Option Component Testing', () => {
+describe('Leave Reports with data', () => {
   beforeEach(() => {
     render(
       <LeaveReportFilterOption
-        selectYear={''}
+        selectYear={'2021'}
         setSelectYear={mockSetSelectYear}
       />,
       {
@@ -70,21 +32,46 @@ describe('Ticket Report Filter Option Component Testing', () => {
       },
     )
   })
-  test('should render Leave Summary Component without crashing', () => {
-    uniqueValue.forEach((childFeature) => {
-      const timeStamp = screen.getByTestId('leave-form-select2')
-      expect(timeStamp).toHaveLength(0)
-    })
+  test('should render search input field', () => {
+    const searchComponent = screen.getByTestId('searchInput')
+    expect(searchComponent).toBeTruthy()
+    const searchInput = screen.findByTestId('searchInput')
+    expect(searchInput).toBeTruthy()
   })
-
+  test('should render export button', () => {
+    const exportComponent = screen.getByTestId('leaveReportExport-btn')
+    expect(exportComponent).toBeTruthy()
+    expect(mockSetSelectYear).toHaveBeenCalledTimes(0)
+  })
+  test('multi search button ', () => {
+    const searchInput = screen.getByPlaceholderText('Search Employees')
+    expect(screen.getByTestId('search-btn1')).toBeDisabled()
+    expect(searchInput).toBeInTheDocument()
+    userEvent.type(searchInput, 'test')
+    expect(searchInput).toHaveValue('test')
+    const searchBtn = screen.getByTestId('search-btn1')
+    userEvent.click(searchBtn)
+    expect(searchBtn).toBeInTheDocument()
+  })
   test('should render search input mouse enter key', () => {
     const searchField = screen.getByTestId('searchInput')
     userEvent.type(searchField, 'testing')
+    expect(searchField).toHaveValue('testing')
     fireEvent.keyDown(searchField, {
       key: 'Enter',
       code: 'Enter',
       charCode: 13,
     })
-    expect(searchField).toHaveValue('testing')
+    expect(mockSetSelectYear).toHaveBeenCalledTimes(0)
+  })
+  test('should render Select year filter', () => {
+    const selectYearFilter = screen.findByTestId('leave-form-select2')
+    expect(selectYearFilter).toBeTruthy()
+  })
+  test('should able to select values for options for respective select element', () => {
+    uniqueValue.forEach((childFeature) => {
+      const selectYear = screen.getByTestId('leave-form-select2')
+      expect(selectYear).toHaveLength(0)
+    })
   })
 })
