5ab1b82382b2bfba06794f35151b63f8b71e3fda
saikirannaik <saikirannayak222@gmail.com>
2022-09-15 19:00:54 +0530

    added types for EventList

---
+++ src/types/ConferenceRoomBooking/EventList/eventListTypes.ts
@@ -0,0 +1,94 @@
+import { LoadingState, ValidationError } from '../../commonTypes'
+
+export type EventListTableProps = {
+  selectMonth: string
+  setSelectMonth: (value: string) => void
+  paginationRange: number[]
+  currentPage: number
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>
+  pageSize: number
+  setPageSize: React.Dispatch<React.SetStateAction<number>>
+}
+
+export type EventListApiProps = {
+  startIndex?: number
+  endIndex?: number
+  dateSelection?: string
+  eventTypeId?: number
+  searchFromDate?: string
+  searchToDate?: string
+}
+
+export type EventListOptions = {
+  selectMonth: string
+  setSelectMonth: (value: string) => void
+}
+
+export type Author = {
+  designation: string
+  emailId: string
+  firstName: string
+  fullName: string
+  id: number
+  lastName: string
+  profilePicPath: string
+}
+
+export type Trainer = {
+  designation: string
+  emailId: string
+  firstName: string
+  fullName: string
+  id: number
+  lastName: string
+  profilePicPath: string
+}
+
+export type Event = {
+  agenda: string
+  authorName: Author
+  availability: null
+  availableDates: null
+  conferenceType: string
+  description: string
+  disableEdit: boolean
+  empDesignations: []
+  employeeAvailability: null
+  employeeDto: []
+  employeeIds: []
+  employeeNames: []
+  endTime: string
+  eventEditAccess: boolean
+  eventId: null
+  eventLocation: string
+  eventTypeId: number
+  eventTypeName: string
+  fromDate: string
+  id: number
+  isAuthorisedUser: boolean
+  locationId: number
+  locationName: string
+  meetingAttendeesDto: null
+  meetingEditDTOList: null
+  meetingStatus: string
+  projectName: null
+  roomId: number
+  roomName: string
+  startTime: string
+  timeFomrat: string
+  toDate: string
+  trainerName: Trainer
+}
+
+export type GetEventListResponse = {
+  list: Event[]
+  size: number
+}
+
+export type EventListSliceState = {
+  events: Event[]
+  selectedMonth: string
+  listSize: number
+  isLoading: LoadingState
+  error: ValidationError
+}
