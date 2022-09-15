ca99d96bbcecd01ad049e7b8851e681eeb9bb479
Vinesh Merugu <vineshmerugu1.com>
2022-09-15 15:18:57 +0530

    Resolved Issues

---
+++ src/pages/MyProfile/ReviewsTab/EmployeeReviewTable.test.tsx
@@ -0,0 +1,37 @@
+import '@testing-library/jest-dom'
+import React from 'react'
+import userEvent from '@testing-library/user-event'
+import EmployeeReviewTable from './EmployeeReviewTable'
+import { render, screen, waitFor } from '../../../test/testUtils'
+import { mockReviewDetails } from '../../../test/data/employeeReviewsData'
+
+const expectPageSizeToBeRendered = (pageSize: number) => {
+  for (let i = 0; i < pageSize; i++) {
+    expect(
+      screen.queryByText(mockReviewDetails[i].employeeName),
+    ).toBeInTheDocument()
+  }
+}
+
+const mockSetCurrentPage = jest.fn()
+const mockSetPageSize = jest.fn()
+
+describe('Scheduled Candidates Table Component Testing', () => {
+  test('should render scheduled candidates table component without crashing', async () => {
+    render(<EmployeeReviewTable />, {
+      preloadedState: {
+        employeeReviews: {
+          employeeReviews: mockReviewDetails,
+        },
+      },
+    })
+
+    expectPageSizeToBeRendered(20)
+
+    await waitFor(() => {
+      userEvent.selectOptions(screen.getByRole('combobox'), ['40'])
+      expect(mockSetPageSize).toHaveBeenCalledTimes(0)
+      expect(mockSetCurrentPage).toHaveBeenCalledTimes(0)
+    })
+  })
+})
---
+++ src/pages/MyProfile/ReviewsTab/EmployeeReviewTable.tsx
@@ -0,0 +1,147 @@
+import {
+  CTable,
+  CTableHead,
+  CTableRow,
+  CTableHeaderCell,
+  CTableBody,
+  CTableDataCell,
+  CRow,
+  CCol,
+  CBadge,
+} from '@coreui/react-pro'
+import React, { useEffect, useMemo } from 'react'
+import OPageSizeSelect from '../../../components/ReusableComponent/OPageSizeSelect'
+import OPagination from '../../../components/ReusableComponent/OPagination'
+import { usePagination } from '../../../middleware/hooks/usePagination'
+import { reduxServices } from '../../../reducers/reduxServices'
+import { useAppDispatch, useTypedSelector } from '../../../stateStore'
+import { currentPageData } from '../../../utils/paginationUtils'
+
+const EmployeeReviewTable = (): JSX.Element => {
+  const employeeId = useTypedSelector(
+    reduxServices.authentication.selectors.selectEmployeeId,
+  )
+  const employeeReviews = useTypedSelector(
+    reduxServices.employeeReviews.selectors.employeeReviews,
+  )
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(reduxServices.employeeReviews.getEmployeeReviews(employeeId))
+  }, [dispatch, employeeId])
+
+  const {
+    paginationRange,
+    setPageSize,
+    setCurrentPage,
+    currentPage,
+    pageSize,
+  } = usePagination(employeeReviews.length, 20)
+
+  useEffect(() => {
+    setPageSize(20)
+  }, [employeeReviews, setPageSize, setCurrentPage])
+
+  const handlePageSizeSelectChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setPageSize(Number(event.target.value))
+    setCurrentPage(1)
+  }
+
+  const getItemNumber = (index: number) => {
+    return (currentPage - 1) * pageSize + index + 1
+  }
+
+  const currentPageItems = useMemo(
+    () => currentPageData(employeeReviews, currentPage, pageSize),
+    [employeeReviews, currentPage, pageSize],
+  )
+
+  return (
+    <>
+      <CTable striped>
+        <CTableHead>
+          <CTableRow>
+            <CTableHeaderCell scope="col">#</CTableHeaderCell>
+            <CTableHeaderCell scope="col">ID</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Employee Name</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Manager Name</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Department</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Designation</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Month</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Emp Avg Rating</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Managers Avg Rating</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Status</CTableHeaderCell>
+          </CTableRow>
+        </CTableHead>
+        <CTableBody>
+          {currentPageItems.map((review, index) => {
+            return (
+              <CTableRow key={index}>
+                <CTableDataCell scope="row">
+                  {getItemNumber(index)}
+                </CTableDataCell>
+                <CTableDataCell scope="row">{review.id}</CTableDataCell>
+                <CTableDataCell scope="row">
+                  {review.employeeName}
+                </CTableDataCell>
+                <CTableDataCell scope="row">
+                  {review.manager1Name}
+                </CTableDataCell>
+                <CTableDataCell scope="row">
+                  {review.empDepartmentName}
+                </CTableDataCell>
+                <CTableDataCell scope="row">
+                  {review.empDesignationName}
+                </CTableDataCell>
+                <CTableDataCell scope="row">
+                  {review.cycleStartDate}
+                </CTableDataCell>
+                <CTableDataCell scope="row">
+                  {review.empAvgRating}
+                </CTableDataCell>
+                <CTableDataCell scope="row">
+                  {review.overallAvgRating || 'N/A'}
+                </CTableDataCell>
+                <CTableDataCell scope="row">
+                  <CBadge color="success"> {review.formStatus}</CBadge>
+                </CTableDataCell>
+              </CTableRow>
+            )
+          })}
+        </CTableBody>
+      </CTable>
+      <CRow>
+        <CCol xs={4} className="mt-3">
+          <strong>
+            {employeeReviews?.length
+              ? `Total Records: ${employeeReviews.length}`
+              : `No Records Found`}
+          </strong>
+        </CCol>
+        <CCol xs={3}>
+          {employeeReviews.length > 20 && (
+            <OPageSizeSelect
+              handlePageSizeSelectChange={handlePageSizeSelectChange}
+              selectedPageSize={pageSize}
+            />
+          )}
+        </CCol>
+        {employeeReviews.length > 20 && (
+          <CCol
+            xs={5}
+            className="d-grid gap-1 d-md-flex justify-content-md-end"
+          >
+            <OPagination
+              currentPage={currentPage}
+              pageSetter={setCurrentPage}
+              paginationRange={paginationRange}
+            />
+          </CCol>
+        )}
+      </CRow>
+    </>
+  )
+}
+export default EmployeeReviewTable
