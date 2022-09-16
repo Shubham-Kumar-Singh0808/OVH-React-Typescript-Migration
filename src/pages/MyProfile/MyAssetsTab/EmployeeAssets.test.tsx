d1e58bebb1dc90e53f910061179f323caf5967dc
Vinesh Merugu <vineshmerugu1.com>
2022-09-17 00:21:56 +0530

    unit test coverge

--- src/pages/MyProfile/MyAssetsTab/EmployeeAssets.test.tsx
+++ src/pages/MyProfile/MyAssetsTab/EmployeeAssets.test.tsx
@@ -1,12 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable import/named */
+// Todd: remove eslint and fix error
+// Todo: remove eslint and fix all the errors
 import '@testing-library/jest-dom'
+import { render, screen, waitFor } from '@testing-library/react'
+import { EnhancedStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 import React from 'react'
+import userEvent from '@testing-library/user-event'
 import EmployeeAssets from './EmployeeAssets'
-import { render, screen } from '../../../test/testUtils'
+import stateStore from '../../../stateStore'
+import { mockAssetsDetails } from '../../../test/data/employeeAssetsData'
 
-describe('Employee Assets Component Testing', () => {
-  test('should render Employee Assets tab component with out crashing', () => {
-    render(<EmployeeAssets />)
+const ReduxProvider = ({
+  children,
+  reduxStore,
+}: {
+  children: JSX.Element
+  reduxStore: EnhancedStore
+}) => <Provider store={reduxStore}>{children}</Provider>
 
-    expect(screen.getByText('My Assets')).toBeInTheDocument()
+const expectPageSizeToBeRendered = (pageSize: number) => {
+  for (let i = 0; i < pageSize; i++) {
+    expect(
+      screen.getByText(mockAssetsDetails[i].employeeName),
+    ).toBeInTheDocument()
+  }
+}
+describe('Assets List Table Testing', () => {
+  test('should render No data to display if Assets is empty', async () => {
+    render(
+      <ReduxProvider reduxStore={stateStore}>
+        <EmployeeAssets />
+      </ReduxProvider>,
+    )
+    await waitFor(() => {
+      expect(screen.getByText('No Records Found...')).toBeInTheDocument()
+    })
+  })
+
+  test('should render correct number of page records', async () => {
+    render(
+      <ReduxProvider reduxStore={stateStore}>
+        <EmployeeAssets />
+      </ReduxProvider>,
+    )
+
+    await waitFor(() => {
+      expect(screen.getAllByRole('row')).toHaveLength(1)
+    })
   })
 })
