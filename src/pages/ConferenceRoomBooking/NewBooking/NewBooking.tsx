86bdf7995a2c58f48cf0a82e4868cab3ce032321
Vinesh Merugu <vineshmerugu1.com>
2022-09-21 12:01:52 +0530

    unit test cases

---
+++ src/pages/ConferenceRoomBooking/NewBooking/NewBooking.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NewBooking = (): JSX.Element => {
+  return <div>NewBooking</div>
+}
+
+export default NewBooking
---
+++ src/pages/ConferenceRoomBooking/NewBooking/NewBookingFilterOptions.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NewBookingFilterOptions = (): JSX.Element => {
+  return <div>NewBookingFilterOptions</div>
+}
+
+export default NewBookingFilterOptions
