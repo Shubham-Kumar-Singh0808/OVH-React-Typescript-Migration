9d4b0242bae3078cafa996c06ad075861156f4ef
Vinesh Merugu <vineshmerugu1.com>
2022-09-29 17:30:01 +0530

    Resolved Issues

--- src/pages/MyProfile/PersonalInfoTab/AddEditVisaDetails.tsx
+++ src/pages/MyProfile/PersonalInfoTab/AddEditVisaDetails.tsx
@@ -163,6 +163,12 @@ function AddEditVisaDetails({
     })
   }
 
+  useEffect(() => {
+    if (employeeVisaDetails?.countryId === '') {
+      dispatch(reduxServices.personalInformation.actions.clearVisaType())
+    }
+  }, [dispatch, employeeVisaDetails?.countryId])
+
   const onChangeDateOfIssueHandler = (date: Date) => {
     const currentDateExpiry = isEditVisaDetails
       ? (employeeVisaDetails.dateOfExpire as string)
