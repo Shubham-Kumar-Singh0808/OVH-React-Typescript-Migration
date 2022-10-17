78058401e129f9214a15acba73012503ad77d6ec
Vinesh Merugu <vineshmerugu1.com>
2022-10-17 16:50:59 +0530

    Resolved Issues

--- src/pages/ConferenceRoomBooking/NewBooking/NewBookingChildComponets/Attendees.tsx
+++ src/pages/ConferenceRoomBooking/NewBooking/NewBookingChildComponets/Attendees.tsx
@@ -1,13 +1,25 @@
 import { CRow, CFormLabel, CCol, CFormInput } from '@coreui/react-pro'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { reduxServices } from '../../../../reducers/reduxServices'
+import { TextDanger, TextWhite } from '../../../../constant/ClassName'
+import { useAppDispatch, useTypedSelector } from '../../../../stateStore'
 
 const Attendees = (): JSX.Element => {
+  const [attendie, setAttendie] = useState<string>('')
+  const allAttendiesNames = useTypedSelector(
+    reduxServices.newBooking.selectors.attendieNames,
+  )
+  console.log(allAttendiesNames)
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    dispatch(reduxServices.newBooking.getAllAttendiesData(attendie))
+  }, [])
   return (
     <>
       <CRow className="mt-1 mb-3">
         <CFormLabel className="col-sm-2 col-form-label text-end">
           Attendees:
-          <span>*</span>
+          <span className={attendie ? TextWhite : TextDanger}>*</span>
         </CFormLabel>
         <CCol sm={4}>
           <CFormInput
@@ -15,6 +27,8 @@ const Attendees = (): JSX.Element => {
             data-testid="selectSubject"
             id="subjectValue"
             name="subjectValue"
+            value={attendie}
+            onChange={(e) => setAttendie(e.target.value)}
           />
         </CCol>
       </CRow>
