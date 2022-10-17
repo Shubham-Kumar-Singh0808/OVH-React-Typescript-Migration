10067b23c9d575178c1ec1fd87b795769d3d8aff
Vinesh Merugu <vineshmerugu1.com>
2022-10-17 13:39:19 +0530

    implemented global spinners

--- src/pages/EmployeeDirectory/EmployeeReport/EmployeeReport.test.tsx
+++ src/pages/EmployeeDirectory/EmployeeReport/EmployeeReport.test.tsx
@@ -4,9 +4,18 @@ import React from 'react'
 import EmployeeReport from './EmployeeReport'
 import { render, screen, waitFor } from '../../../test/testUtils'
 
+const toRender = (
+  <div>
+    <div id="backdrop-root"></div>
+    <div id="overlay-root"></div>
+    <div id="root"></div>
+    <EmployeeReport />
+  </div>
+)
+
 describe('Employee Report Component Testing', () => {
   test('should render Employee Report component without crashing', async () => {
-    render(<EmployeeReport />)
+    render(toRender)
 
     await waitFor(() => {
       expect(screen.getByText('Employee Report')).toBeInTheDocument()
