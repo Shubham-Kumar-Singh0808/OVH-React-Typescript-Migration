a74e896099bf569f4591cffbdd8bff307e8c7653
vinesh <vineshmerugu1@gmail.com>
2022-11-04 18:03:53 +0530

    Resolved Bug

---
+++ src/pages/Settings/LeaveSettings/EmployeeLeaveSettings.test.tsx
@@ -0,0 +1,20 @@
+import '@testing-library/jest-dom'
+import React from 'react'
+import EmployeeLeaveSettings from './EmployeeLeaveSettings'
+import { render, screen } from '../../../test/testUtils'
+
+const toRender = (
+  <div>
+    <div id="backdrop-root"></div>
+    <div id="overlay-root"></div>
+    <div id="root"></div>
+    <EmployeeLeaveSettings />,
+  </div>
+)
+
+describe('Leave Settings Component Testing', () => {
+  test('should render Leave Settings component with out crashing', () => {
+    render(toRender)
+    expect(screen.getByText('Leave Settings')).toBeInTheDocument()
+  })
+})
