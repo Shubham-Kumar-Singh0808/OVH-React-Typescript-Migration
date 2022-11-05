8758f9fffa398357c2a3829157c9e0d60710c718
vinesh <vineshmerugu1@gmail.com>
2022-11-05 17:44:24 +0530

    Rebae

---
+++ src/pages/Settings/Configuration/AddConfiguration/ConfigurationFilterOption.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ConfigurationFilterOption = (): JSX.Element => {
+  return (
+    <>
+      <h1>Welcome</h1>
+    </>
+  )
+}
+export default ConfigurationFilterOption
