248d0d17345c670ded4506cce9128fb91f308ebe
Sai Kiran Banothu <saikiran.banothu@raybusiness.onmicrosoft.com>
2022-11-09 19:51:45 +0530

    fixed roles and permissions for dashboard upcoming trainings, upcoming events and Achievements

--- src/pages/Dashboard/Trainings/UpcomingTrainings.tsx
+++ src/pages/Dashboard/Trainings/UpcomingTrainings.tsx
@@ -12,43 +12,54 @@ const UpcomingTrainings = (): JSX.Element => {
     reduxServices.trainingsAndEvents.selectors.isLoading,
   )
 
+  const userAccessToFeatures = useTypedSelector(
+    reduxServices.userAccessToFeatures.selectors.userAccessToFeatures,
+  )
+  const userAccessToUpcomingTrainings = userAccessToFeatures?.find(
+    (feature) => feature.name === 'Upcoming Trainings',
+  )
+
   return (
     <>
-      {isTrainingLoading !== ApiLoadingState.loading &&
-      upcomingTrainings?.length ? (
+      {userAccessToUpcomingTrainings?.viewaccess && (
         <>
-          <div className="holidays-panel-body ps-0 pe-0">
-            <ul className="holidays-list-group recent-comments mb0">
-              {upcomingTrainings?.slice(0, 3).map((training, index) => {
-                return (
-                  <li
-                    className="birthdays-list-group-item clearfix"
-                    key={index}
-                  >
-                    <p className="text-ellipsis mb0">
-                      <span className="strong">{training.agenda}</span>
-                    </p>
-                    <span className="pull-right"></span>
-                    <span>{training.locationName}</span>
-                    <span>{training.roomName}</span>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-          {upcomingTrainings?.length !== 0 && (
-            <div className="panel-footer mbtrl0">
-              <p className="text-right mb0">
-                <a href="/eventList">
-                  More {''}
-                  <i className="fa fa-angle-double-right fa-lg"></i>
-                </a>
-              </p>
-            </div>
+          {isTrainingLoading !== ApiLoadingState.loading &&
+          upcomingTrainings?.length ? (
+            <>
+              <div className="holidays-panel-body ps-0 pe-0">
+                <ul className="holidays-list-group recent-comments mb0">
+                  {upcomingTrainings?.slice(0, 3).map((training, index) => {
+                    return (
+                      <li
+                        className="birthdays-list-group-item clearfix"
+                        key={index}
+                      >
+                        <p className="text-ellipsis mb0">
+                          <span className="strong">{training.agenda}</span>
+                        </p>
+                        <span className="pull-right"></span>
+                        <span>{training.locationName}</span>
+                        <span>{training.roomName}</span>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
+              {upcomingTrainings?.length !== 0 && (
+                <div className="panel-footer mbtrl0">
+                  <p className="text-right mb0">
+                    <a href="/eventList">
+                      More {''}
+                      <i className="fa fa-angle-double-right fa-lg"></i>
+                    </a>
+                  </p>
+                </div>
+              )}
+            </>
+          ) : (
+            <strong className="ml14">No Records Found...</strong>
           )}
         </>
-      ) : (
-        <strong className="ml14">No Records Found...</strong>
       )}
     </>
   )
