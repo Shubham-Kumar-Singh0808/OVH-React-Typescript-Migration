25a569b9273ddf8f84df94268656320c3458d84f
Vinesh Merugu <vineshmerugu1.com>
2022-11-18 17:17:14 +0530

    Resolved Issues

--- src/pages/ProjectManagement/EmployeeAllocation/EmployeeAllocationEntryTable.tsx
+++ src/pages/ProjectManagement/EmployeeAllocation/EmployeeAllocationEntryTable.tsx
@@ -139,6 +139,20 @@ const EmployeeAllocationEntryTable = (props: {
           />,
         ),
       )
+      dispatch(
+        reduxServices.employeeAllocationReport.getEmployeeAllocationReport({
+          Billingtype: billingStatus,
+          EmployeeStatus: '',
+          dateSelection: Select,
+          departmentNames: [],
+          employeeName: '',
+          endIndex: 20,
+          enddate: '',
+          firstIndex: 0,
+          startdate: '',
+          technology: '',
+        }),
+      )
     }
   }
 
