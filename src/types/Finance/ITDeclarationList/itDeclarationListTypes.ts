2816df877dcc08aa44f86b798a6a1704406bd148
Sai Kiran Banothu <saikiran.banothu@raybusiness.onmicrosoft.com>
2022-11-29 09:58:34 +0530

    added types for table api

--- src/types/Finance/ITDeclarationList/itDeclarationListTypes.ts
+++ src/types/Finance/ITDeclarationList/itDeclarationListTypes.ts
@@ -8,6 +8,41 @@ export type Cycle = {
   startDate: string
 }
 
+export type FormInvestment = {
+  formInvestmentId: number
+  investmentId: number
+  investmentName: string
+  customAmount: number
+}
+
+export type FormSection = {
+  isOld: boolean
+  itSectionsId: number
+  maxLimit: number
+  sectionId: number
+  sectionName: string
+  formInvestmentDTO: FormInvestment[]
+}
+export type ITForm = {
+  fromDate: string
+  grandTotal: number
+  isAgree: null
+  itDeclarationFormId: number
+  organisationName: string
+  panNumber: string
+  toDate: string
+  cycleId: number
+  designation: string
+  employeeId: number
+  employeeName: string
+  filePath: null
+  formSectionsDTOs: FormSection[]
+}
+
+export type ITDeclarationFormListResponse = {
+  itformlistsize: number
+  itforms: ITForm[]
+}
 export type ITDeclarationListSliceState = {
   isLoading: LoadingState
   error: ValidationError
