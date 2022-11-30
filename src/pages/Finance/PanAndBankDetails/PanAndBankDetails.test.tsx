982f6971e10395737c59924d7984024dd9c9889d
Jyothika Devi <jyothikadevi.goru@raybusiness.onmicrosft.com>
2022-11-30 11:31:25 +0530

    Added testcases

--- src/pages/Finance/PanAndBankDetails/PanAndBankDetails.test.tsx
+++ src/pages/Finance/PanAndBankDetails/PanAndBankDetails.test.tsx
@@ -1,6 +1,9 @@
 import '@testing-library/jest-dom'
 import React from 'react'
+import userEvent from '@testing-library/user-event'
 import PanAndBankDetails from './PanAndBankDetails'
+import AddBankAccount from './BankDetails/AddBankAccount'
+import EditBankAccount from './BankDetails/EditBankAccount'
 import { render, screen } from '../../../test/testUtils'
 
 const toRender = (
@@ -15,5 +18,27 @@ describe('Pan And Bank Details Component Testing', () => {
   test('should render Pan And Bank Details component with out crashing', () => {
     render(toRender)
     expect(screen.getByText('P.F. & PAN Details')).toBeInTheDocument()
+  })
+})
+
+const mockSetToggle = jest.fn()
+
+describe('Add Bank Account Component Testing', () => {
+  test('should render Edit Bank Account component with out crashing', () => {
+    render(<AddBankAccount backButtonHandler={mockSetToggle} />)
+    const backButtonElement = screen.getByTestId('back-button')
+    expect(backButtonElement).toBeInTheDocument()
+    userEvent.click(backButtonElement)
+    expect(mockSetToggle).toHaveBeenCalledTimes(1)
+  })
+})
+
+describe('Edit Bank Account Component Testing', () => {
+  test('should render Edit Bank Account component with out crashing', () => {
+    render(<EditBankAccount backButtonHandler={mockSetToggle} />)
+    const backButtonElement = screen.getByTestId('back-button')
+    expect(backButtonElement).toBeInTheDocument()
+    userEvent.click(backButtonElement)
+    expect(mockSetToggle).toHaveBeenCalledTimes(1)
   })
 })
