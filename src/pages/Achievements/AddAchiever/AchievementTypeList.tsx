65cff6bee499c7a1fc489c39a901ec3660ec354f
Pranav Gupta <pranav.gupta@raybusiness.onmicrosoft.com>
2022-12-13 18:22:19 +0530

    changes

--- src/pages/Achievements/AddAchiever/AchievementTypeList.tsx
+++ src/pages/Achievements/AddAchiever/AchievementTypeList.tsx
@@ -1,7 +1,30 @@
+import {
+  CCol,
+  CContainer,
+  CForm,
+  CFormLabel,
+  CFormSelect,
+  CRow,
+} from '@coreui/react-pro'
 import React from 'react'
 
 const AchievementTypeList = () => {
-  return <div>Achievement Type List</div>
+  return (
+    <CForm>
+      <CContainer className="mt-4 ms-2">
+        <CRow>
+          <div className="d-flex flex-row align-items-center">
+            <CFormLabel>Select Achievement Type</CFormLabel>
+            <CCol md={3}>
+              <CFormSelect value={'hi'}>
+                <option value={'Hi there'}>Hi there</option>
+              </CFormSelect>
+            </CCol>
+          </div>
+        </CRow>
+      </CContainer>
+    </CForm>
+  )
 }
 
 export default AchievementTypeList
