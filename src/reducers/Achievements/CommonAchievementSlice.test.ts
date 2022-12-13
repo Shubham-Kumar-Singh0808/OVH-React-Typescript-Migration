414fe85d21e65e3f899dec055fd298c7ec95d43a
Pranav Gupta <pranav.gupta@raybusiness.onmicrosoft.com>
2022-12-13 16:55:40 +0530

    Achiever List Sub Module

---
+++ src/reducers/Achievements/CommonAchievementSlice.test.ts
@@ -0,0 +1,36 @@
+import commonAchievementsReducer, {
+  commonAchievementsService,
+} from './CommonAchievementsSlice'
+import { ApiLoadingState } from '../../middleware/api/apiList'
+import { commonAchievementsInitialState } from '../../types/Achievements/commonAchievementTypes'
+import { mockAchievementTypeList } from '../../test/data/AchieverListData'
+
+describe('Common Achievement Slice', () => {
+  describe('Common Achievement Reducer', () => {
+    const initialState: commonAchievementsInitialState = {
+      isLoading: ApiLoadingState.idle,
+      achievementTypeList: { size: 0, list: [] },
+    }
+    it('"isLoading" should be set to "loading" for "getAllAchievementsType"', () => {
+      const action = {
+        type: commonAchievementsService.getAllAchievementsType.pending.type,
+      }
+      const state = commonAchievementsReducer(initialState, action)
+      expect(state).toEqual({
+        isLoading: ApiLoadingState.loading,
+        achievementTypeList: { size: 0, list: [] },
+      })
+    })
+
+    it('"isLoading" should be set to "succeeded" for "getAllAchievementsType"', () => {
+      const action = {
+        type: commonAchievementsService.getAllAchievementsType.fulfilled.type,
+      }
+      const state = commonAchievementsReducer(initialState, action)
+      expect(state).toEqual({
+        isLoading: ApiLoadingState.succeeded,
+        achievementTypeList: mockAchievementTypeList,
+      })
+    })
+  })
+})
