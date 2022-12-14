47995d0a3b0ccee939d13450341de600e45f54b9
Vinesh Merugu <vineshmerugu1.com>
2022-12-14 18:14:16 +0530

    Resolvbed Issues

--- src/pages/Separation/ResignationList/ResignationTimeLine/ResignationHistory.tsx
+++ src/pages/Separation/ResignationList/ResignationTimeLine/ResignationHistory.tsx
@@ -18,6 +18,14 @@ const ResignationHistory = (): JSX.Element => {
     reduxServices.resignationList.selectors.resignationTimeLine,
   )
 
+  const userAccessToFeatures = useTypedSelector(
+    reduxServices.userAccessToFeatures.selectors.userAccessToFeatures,
+  )
+
+  const userAccessEditTimeLine = userAccessToFeatures?.find(
+    (feature) => feature.name === 'Separation',
+  )
+
   return (
     <>
       <OCard
@@ -28,14 +36,14 @@ const ResignationHistory = (): JSX.Element => {
       >
         <CRow className="justify-content-end">
           <CCol className="text-end" md={4}>
-            {getAllResignationHistory.status === 'Relieved' ? (
+            {getAllResignationHistory.status === 'Relieved' &&
+            userAccessEditTimeLine?.updateaccess ? (
               ''
             ) : (
               <CButton color="info" className="btn-ovh me-1">
                 <i className="fa fa-arrow-left  me-1"></i>Edit
               </CButton>
             )}
-
             <Link to={`/resignationList`}>
               <CButton color="info" className="btn-ovh me-1">
                 <i className="fa fa-arrow-left  me-1"></i>Back
