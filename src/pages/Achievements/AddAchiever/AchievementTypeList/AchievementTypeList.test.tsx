cf47d715b198a78d3a18810d3bf55f68f54e37fc
Pranav Gupta <pranav.gupta@raybusiness.onmicrosoft.com>
2022-12-17 12:23:46 +0530

    c

--- src/pages/Achievements/AddAchiever/AchievementTypeList/AchievementTypeList.test.tsx
+++ src/pages/Achievements/AddAchiever/AchievementTypeList/AchievementTypeList.test.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom'
 
 import React from 'react'
+import userEvent from '@testing-library/user-event'
 import AchievementTypeList from './AchievementTypeList'
 import { cleanup, fireEvent, render, screen } from '../../../../test/testUtils'
 import { mockAchievementTypeList } from '../../../../test/data/AchieverListData'
@@ -45,6 +46,49 @@ describe('Achievement Type List Testing', () => {
     test('scroll functionality not enabled', () => {
       const col = screen.getByTestId('scroll-col')
       expect(col).not.toHaveClass('custom-scroll')
+    })
+    //Wrote these tests again here to meet sonar requirements and all functions are included as this is the parent file
+    test('test clear button', () => {
+      const achName = screen.getByTestId('ach-name-input')
+      const clearBtn = screen.getByTestId('clear-btn-id')
+      userEvent.type(achName, 'testing done right')
+      userEvent.click(clearBtn)
+      expect(achName).toHaveValue('')
+      expect(screen.getByTestId('ach-order-input')).toHaveValue('')
+    })
+    test('test add button', () => {
+      const addBtn = screen.getByTestId('add-btn-id')
+      expect(addBtn).toBeDisabled()
+      const achName = screen.getByTestId('ach-name-input')
+      const achOrder = screen.getByTestId('ach-order-input')
+      userEvent.type(achName, 'testing on')
+      userEvent.type(achOrder, '99')
+      expect(addBtn).toBeEnabled()
+      userEvent.click(addBtn)
+      expect(
+        screen.findByText('Achievement Type Added Successfully'),
+      ).toBeTruthy()
+    })
+    test('test save changes button', () => {
+      const editBtn = screen.getByTestId('edit-btn-1')
+      userEvent.click(editBtn)
+      const saveBtn = screen.getByTestId('save-btn-1')
+      expect(saveBtn).toBeVisible()
+      const order = screen.getByTestId('new-order')
+      userEvent.clear(order)
+      userEvent.type(order, '5')
+      userEvent.click(saveBtn)
+    })
+    test('test delete button', () => {
+      const editBtn = screen.getByTestId('del-btn-0')
+      userEvent.click(editBtn)
+      const modalContent = screen.getByTestId('confirm-modal-content')
+      expect(modalContent).toBeTruthy()
+      expect(modalContent).toHaveTextContent(
+        'Do you really want to delete 12Nov20221 type?',
+      )
+      const confirmModalButton = screen.getByTestId('modalConfirmBtn')
+      userEvent.click(confirmModalButton)
     })
   })
 })
