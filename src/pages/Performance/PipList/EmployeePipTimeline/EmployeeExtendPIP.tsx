e62b609595c6223e3cec600147d9ed191e3b6f16
Jyothika Devi <jyothikadevi.goru@raybusiness.onmicrosft.com>
2023-01-11 16:33:04 +0530

    Added Update and Extend and Remove PIP Data Integartions

---
+++ src/pages/Performance/PipList/EmployeePipTimeline/EmployeeExtendPIP.tsx
@@ -0,0 +1,35 @@
+import { CRow, CCol, CButton } from '@coreui/react-pro'
+import React from 'react'
+import OCard from '../../../../components/ReusableComponent/OCard'
+
+const EmployeeExtendPIP = ({
+  setToggle,
+}: {
+  setToggle: (value: string) => void
+}): JSX.Element => {
+  return (
+    <>
+      <OCard
+        className="mb-4 myprofile-wrapper"
+        title={'Extend PIP'}
+        CBodyClassName="ps-0 pe-0"
+        CFooterClassName="d-none"
+      >
+        <CRow className="justify-content-end">
+          <CCol className="text-end" md={4}>
+            <CButton
+              color="info"
+              className="btn-ovh me-1"
+              data-testid="back-button"
+              onClick={() => setToggle('')}
+            >
+              <i className="fa fa-arrow-left  me-1"></i>Back
+            </CButton>
+          </CCol>
+        </CRow>
+      </OCard>
+    </>
+  )
+}
+
+export default EmployeeExtendPIP
--- src/pages/Performance/PipList/EmployeePipTimeline/EmployeePipTimeline.tsx
+++ src/pages/Performance/PipList/EmployeePipTimeline/EmployeePipTimeline.tsx
@@ -1,94 +1,73 @@
-import { CRow, CCol, CButton, CBadge } from '@coreui/react-pro'
-import React from 'react'
+import { CRow, CCol, CButton } from '@coreui/react-pro'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import EmployeePIPDetails from './EmployeePIPDetails'
+import EmployeeExtendPIP from './EmployeeExtendPIP'
+import EmployeeRemovePIP from './EmployeeRemovePIP'
+import EmployeeUpdatePIP from './EmployeeUpdatePIP'
 import OCard from '../../../../components/ReusableComponent/OCard'
-import { reduxServices } from '../../../../reducers/reduxServices'
-import { useTypedSelector } from '../../../../stateStore'
 
 const EmployeePipTimeline = (): JSX.Element => {
-  const employeePIPTimeline = useTypedSelector(
-    reduxServices.pipList.selectors.employeePIPTimeline,
-  )
+  const [toggle, setToggle] = useState<string>('')
 
-  const isPersistValue = (persistType: string) => {
-    if (persistType === 'UPDATED') {
-      return (
-        <CBadge className="rounded-pill" color="info" data-testid="update-btn">
-          Updated
-        </CBadge>
-      )
-    } else if (persistType === 'CREATED') {
-      return (
-        <CBadge
-          className="rounded-pill"
-          color="success"
-          data-testid="created-btn"
-        >
-          Created
-        </CBadge>
-      )
-    } else if (persistType === 'CANCELLED') {
-      return (
-        <CBadge className="rounded-pill" color="warning">
-          Cancelled
-        </CBadge>
-      )
-    } else if (persistType === 'REJECTED') {
-      return (
-        <CBadge className="rounded-pill" color="danger">
-          Rejected
-        </CBadge>
-      )
-    } else {
-      return ''
-    }
-  }
   return (
     <>
-      <OCard
-        className="mb-4 myprofile-wrapper"
-        title="PIP Details"
-        CBodyClassName="ps-0 pe-0"
-        CFooterClassName="d-none"
-      >
-        <CRow className="justify-content-end">
-          <CCol className="text-end" md={4}>
-            <CButton
-              color="info"
-              className="btn-ovh me-1"
-              data-testid="toggle-back-btn"
-            >
-              <i className="fa fa-arrow-left  me-1"></i>Back
-            </CButton>
-          </CCol>
-        </CRow>
-        <div className="sh-timeline-container">
-          {employeePIPTimeline.map((pipDetails, index) => {
-            return (
-              <div key={index} className="sh-timeline-card">
-                <div
-                  className="sh-timeline-timestamp"
-                  data-testid="sh-time-stamp"
+      {toggle === '' && (
+        <OCard
+          className="mb-4 myprofile-wrapper"
+          title="PIP Details"
+          CBodyClassName="ps-0 pe-0"
+          CFooterClassName="d-none"
+        >
+          <CRow className="justify-content-end">
+            <CCol className="text-end" md={5}>
+              <CButton
+                data-testid="save-btn"
+                className="btn-ovh me-1 text-white"
+                color="success"
+                onClick={() => setToggle('employeeUpdatePIP')}
+              >
+                Update
+              </CButton>
+              <CButton
+                data-testid="clear-btn"
+                color="warning"
+                className="btn-ovh me-1 text-white"
+                onClick={() => setToggle('employeeExtendPIP')}
+              >
+                Extend PIP
+              </CButton>
+              <CButton
+                data-testid="save-btn"
+                className="btn-ovh me-1 text-white"
+                color="success"
+                onClick={() => setToggle('employeeRemovePIP')}
+              >
+                Remove From PIP
+              </CButton>
+              <Link to={`/PIPList`}>
+                <CButton
+                  color="info"
+                  className="btn-ovh me-1"
+                  data-testid="toggle-back-btn"
                 >
-                  {pipDetails.modifiedDate}
-                </div>
-                <div className="sh-timeline-content">
-                  <div
-                    className="sh-timeline-header mb-4 clearfix"
-                    data-testid="sh-modifiedBy"
-                  >
-                    <h4 className="sh-timeline-title">
-                      {pipDetails.modifiedBy} -
-                    </h4>
-                    <span className="sh-timeline-status">
-                      {isPersistValue(pipDetails.persistType)}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            )
-          })}
-        </div>
-      </OCard>
+                  <i className="fa fa-arrow-left  me-1"></i>Back
+                </CButton>
+              </Link>
+            </CCol>
+          </CRow>
+          <EmployeePIPDetails />
+        </OCard>
+      )}
+      {toggle === 'employeeExtendPIP' && (
+        <EmployeeExtendPIP setToggle={setToggle} />
+      )}
+      {toggle === 'employeeRemovePIP' && (
+        <EmployeeRemovePIP setToggle={setToggle} />
+      )}
+      {toggle === 'employeeUpdatePIP' && (
+        <EmployeeUpdatePIP setToggle={setToggle} />
+      )}
     </>
   )
 }
