d9de500ff3e840b7a1d92a2385214bdf6fdd9c3a
Vinesh Merugu <vineshmerugu1.com>
2023-01-12 13:16:17 +0530

    Resolved Issues

--- src/pages/ProjectManagement/Project/ProjectView/ProjectViewLandingPage/ProjectViewTabs.tsx
+++ src/pages/ProjectManagement/Project/ProjectView/ProjectViewLandingPage/ProjectViewTabs.tsx
@@ -7,6 +7,10 @@ import {
 } from '@coreui/react-pro'
 import React, { useEffect, useState } from 'react'
 import ProjectViewTabLabel from '../../../../../middleware/ProjectViewTabLables'
+import { reduxServices } from '../../../../../reducers/reduxServices'
+import { useTypedSelector } from '../../../../../stateStore'
+import { MappedTabs } from '../../../../../types/MyProfile/ProfileLandingPage/myProfileTabsTypes'
+import { mapTabsToFeatures } from '../../../../../utils/helper'
 import ChangeRequest from '../ChangeRequest/ChangeRequest'
 import ProjectInvoices from '../Invoices/ProjectInvoices'
 import MileStone from '../MileStone/MileStone'
@@ -25,10 +29,39 @@ interface ShowTabContentType<TValue> {
 const ProjectViewTabs = (): JSX.Element => {
   const [activeTabsKey, setActiveTabsKey] = useState(1)
   const [activeTabsContent, setActiveTabsContent] = useState<JSX.Element>()
+  const [tabResult, setTabResult] = useState<MappedTabs[]>()
   const handleActiveTab = (tabKey: number) => {
     setActiveTabsKey(tabKey)
   }
 
+  const employeeRole = useTypedSelector(
+    reduxServices.authentication.selectors.selectEmployeeRole,
+  )
+  const userAccessToFeatures = useTypedSelector(
+    reduxServices.userAccessToFeatures.selectors.userAccessToFeatures,
+  )
+  useEffect(() => {
+    const filteredTabs = userAccessToFeatures.filter((feature) =>
+      ProjectViewTabLabel.some(
+        (tab) =>
+          tab.label.toLowerCase() === feature.name?.toLowerCase() &&
+          feature.viewaccess === true,
+      ),
+    )
+    const mappedTabs = mapTabsToFeatures(ProjectViewTabLabel, filteredTabs)
+    setTabResult(mappedTabs as MappedTabs[])
+  }, [userAccessToFeatures])
+
+  useEffect(() => {
+    if (tabResult) {
+      const newTabResult = tabResult?.filter(
+        (value) => Object.keys(value).length !== 0,
+      )
+      if (newTabResult) {
+        setActiveTabsKey(newTabResult[0]?.id)
+      }
+    }
+  }, [tabResult])
   useEffect(() => {
     const changeTabContent = (tabKey: number): JSX.Element => {
       const showTabContent: ShowTabContentType<JSX.Element> = {
@@ -48,24 +81,27 @@ const ProjectViewTabs = (): JSX.Element => {
     }
 
     setActiveTabsContent(changeTabContent(activeTabsKey))
-  }, [activeTabsKey])
+  }, [activeTabsKey, employeeRole])
 
   return (
     <>
       <CNav className="inline-tabs-nav" variant="tabs" role="tablist">
-        {ProjectViewTabLabel.map((item) => (
-          <CNavItem key={item.id}>
-            <>
-              <CNavLink
-                active={activeTabsKey === item.id}
-                onClick={() => handleActiveTab(item.id)}
-                data-testid="navLink-test"
-              >
-                {item.tabName}
-              </CNavLink>
-            </>
-          </CNavItem>
-        ))}
+        {tabResult
+          ?.filter((value) => Object.keys(value).length !== 0)
+          .map((item, index) => (
+            <CNavItem key={index}>
+              <>
+                <CNavLink
+                  data-testid="profileNavLink"
+                  active={activeTabsKey === item.id}
+                  onClick={() => handleActiveTab(item.id)}
+                >
+                  {item.tabName}
+                </CNavLink>
+              </>
+              {/* )} */}
+            </CNavItem>
+          ))}
       </CNav>
       <CTabContent className="inline-tabs-content">
         <CTabPane
