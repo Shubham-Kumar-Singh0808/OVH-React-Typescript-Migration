eeea4d8d7d37b5cf25dfd9529aa182853e1eefe7
Jyothika Devi <jyothikadevi.goru@raybusiness.onmicrosft.com>
2023-01-19 11:45:56 +0530

    Removed testcases

--- src/pages/Performance/PipList/EmployeePIPClearenceCertificate/EmployeePIPClearenceCertificate.test.tsx
+++ src/pages/Performance/PipList/EmployeePIPClearenceCertificate/EmployeePIPClearenceCertificate.test.tsx
@@ -9,8 +9,4 @@ describe('Exit FeedBack form Component Testing', () => {
 
     expect(screen.getByText('Exit FeedBack Form')).toBeInTheDocument()
   })
-  test('should render add PIP component with out crashing', () => {
-    expect(screen.getByRole('button', { name: 'Submit' })).toBeInTheDocument()
-    expect(screen.getByRole('button', { name: 'Clear' })).toBeInTheDocument()
-  })
 })
