4ab02e50c253403cb403a3f4b70b7de20436f2d7
Jyothika Devi <jyothikadevi.goru@raybusiness.onmicrosft.com>
2023-01-19 13:06:52 +0530

    Added Update testcases

--- src/pages/Performance/PipList/EmployeePipTimeline/EmployeeUpdatePIP.test.tsx
+++ src/pages/Performance/PipList/EmployeePipTimeline/EmployeeUpdatePIP.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
+import { CKEditor } from 'ckeditor4-react'
 import EmployeeUpdatePIP from './EmployeeUpdatePIP'
 import { ApiLoadingState } from '../../../../middleware/api/apiList'
 import { render, screen } from '../../../../test/testUtils'
@@ -37,5 +38,22 @@ describe('should render Employee Pip Time line Component without data', () => {
     const selectRating = screen.getByTestId('form-select1')
     userEvent.selectOptions(selectRating, ['5'])
     expect(selectRating).toHaveValue('5')
+  })
+  test('should render with UpdateEmployeeName ', () => {
+    const subject = screen.getByTestId('UpdateEmployeeName')
+    userEvent.type(subject, 'Ashish  kumar Kashyap kumar')
+    expect(subject).toHaveValue('Ashish  kumar Kashyap kumar')
+  })
+  test('pass comments to test input value', () => {
+    render(
+      <CKEditor
+        initData={process.env.JEST_WORKER_ID !== undefined && <p>Test</p>}
+      />,
+    )
+  })
+  test('should be able to click Add button element', () => {
+    const addBtn = screen.getByRole('button', { name: 'Update' })
+    userEvent.click(addBtn)
+    expect(addBtn).toBeInTheDocument()
   })
 })
