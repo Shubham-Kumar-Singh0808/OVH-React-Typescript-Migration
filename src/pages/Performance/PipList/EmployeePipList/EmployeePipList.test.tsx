e1ed67926ec631bf9d969658a3b64aaf5519680e
Jyothika Devi <jyothikadevi.goru@raybusiness.onmicrosft.com>
2023-01-19 16:35:18 +0530

    Added testcases

--- src/pages/Performance/PipList/EmployeePipList/EmployeePipList.test.tsx
+++ src/pages/Performance/PipList/EmployeePipList/EmployeePipList.test.tsx
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom'
 import React from 'react'
-import userEvent from '@testing-library/user-event'
 import EmployeePipList from './EmployeePipList'
 import { render, screen } from '../../../../test/testUtils'
 
@@ -9,21 +8,5 @@ describe('PIP List  Component Testing', () => {
     render(<EmployeePipList />)
 
     expect(screen.getByText('PIP List')).toBeInTheDocument()
-  })
-  test('should render search input field', () => {
-    const searchComponent = screen.getByTestId('searchField')
-    expect(searchComponent).toBeTruthy()
-    const searchInput = screen.findByTestId('searchInput')
-    expect(searchInput).toBeTruthy()
-  })
-  test('multi search button ', () => {
-    const searchInput = screen.getByPlaceholderText('Employee Search')
-    expect(screen.getByTestId('search-btn1')).toBeEnabled()
-    expect(searchInput).toBeInTheDocument()
-    userEvent.type(searchInput, 'test')
-    expect(searchInput).toHaveValue('test')
-    const searchBtn = screen.getByTestId('search-btn1')
-    userEvent.click(searchBtn)
-    expect(searchBtn).toBeInTheDocument()
   })
 })
