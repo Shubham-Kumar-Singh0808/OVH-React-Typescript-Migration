8b5714d57d21b1c9294f4c17f8900974a783515e
Jyothika Devi <jyothikadevi.goru@raybusiness.onmicrosft.com>
2023-02-02 15:24:04 +0530

    Add process area

--- src/pages/Settings/ProcessArea/ProcessArea.tsx
+++ src/pages/Settings/ProcessArea/ProcessArea.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { CRow, CCol, CFormLabel, CFormSelect } from '@coreui/react-pro'
+import React, { useEffect } from 'react'
 import OCard from '../../../components/ReusableComponent/OCard'
+import { reduxServices } from '../../../reducers/reduxServices'
+import { useAppDispatch } from '../../../stateStore'
 
-const ProcessArea = () => {
+const ProcessArea = (): JSX.Element => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(reduxServices.roomLists.getMeetingRooms())
+    dispatch(reduxServices.addLocationList.getAllMeetingLocationsData())
+  }, [dispatch])
   return (
     <>
       <OCard
@@ -9,7 +18,24 @@ const ProcessArea = () => {
         title="Process Area List"
         CBodyClassName="ps-0 pe-0"
         CFooterClassName="d-none"
-      ></OCard>
+      >
+        <CRow className="mb-3 mt-3">
+          <CCol sm={2} md={1} className="text-end">
+            <CFormLabel className="mt-1">Category: </CFormLabel>
+          </CCol>
+          <CCol sm={3}>
+            <CFormSelect
+              aria-label="Default select example"
+              size="sm"
+              id="location"
+              data-testid="form-select1"
+              name="location"
+            >
+              <option value={''}>-- Select Category --</option>
+            </CFormSelect>
+          </CCol>
+        </CRow>
+      </OCard>
     </>
   )
 }
