828232c753de601f699abc8e8008eaa8c6b9f14d
vinesh <vineshmerugu1@gmail.com>
2023-02-06 01:04:28 +0530

    Resolved Issues

--- src/pages/SQAAuditReport/SQAAuditReport.tsx
+++ src/pages/SQAAuditReport/SQAAuditReport.tsx
@@ -1,20 +1,8 @@
 import React from 'react'
+import SQAAuditReportFilterOptions from './SQAAuditReportFilterOptions'
 import OCard from '../../components/ReusableComponent/OCard'
-import { usePagination } from '../../middleware/hooks/usePagination'
-import { reduxServices } from '../../reducers/reduxServices'
-import { useTypedSelector } from '../../stateStore'
 
 const SQAAuditReport = (): JSX.Element => {
-  const auditlistSize = useTypedSelector(
-    reduxServices.tickets.selectors.allTicketsListSize,
-  )
-  const {
-    paginationRange,
-    setPageSize,
-    setCurrentPage,
-    currentPage,
-    pageSize,
-  } = usePagination(listSize, 20)
   return (
     <>
       <OCard
@@ -22,7 +10,9 @@ const SQAAuditReport = (): JSX.Element => {
         title="SQA Audit Report"
         CBodyClassName="ps-0 pe-0"
         CFooterClassName="d-none"
-      ></OCard>
+      >
+        <SQAAuditReportFilterOptions />
+      </OCard>
     </>
   )
 }
--- src/pages/SQAAuditReport/SQAAuditReportFilterOptions.tsx
+++ src/pages/SQAAuditReport/SQAAuditReportFilterOptions.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import SQAAuditReportTable from './SQAAuditReportTable'
+import { usePagination } from '../../middleware/hooks/usePagination'
+import { reduxServices } from '../../reducers/reduxServices'
+import { useAppDispatch, useTypedSelector } from '../../stateStore'
 
 const SQAAuditReportFilterOptions = (): JSX.Element => {
-  return <div>SQAAuditReportFilterOptions</div>
+  const sqaAuditReportListSize = useTypedSelector(
+    reduxServices.sqaAuditReport.selectors.sqaAuditReportListSize,
+  )
+  const dispatch = useAppDispatch()
+  const {
+    paginationRange,
+    setPageSize,
+    setCurrentPage,
+    currentPage,
+    pageSize,
+  } = usePagination(sqaAuditReportListSize, 20)
+
+  useEffect(() => {
+    dispatch(
+      reduxServices.sqaAuditReport.getSQAAuditReport({
+        endIndex: pageSize * currentPage,
+        multiSearch: '',
+        startIndex: pageSize * (currentPage - 1),
+        SQAAuditSelectionDate: '',
+        auditRescheduleStatus: '',
+        auditStatus: '',
+        from: '',
+        to: '',
+      }),
+    )
+  }, [dispatch, pageSize, currentPage])
+  return (
+    <>
+      <SQAAuditReportTable
+        paginationRange={paginationRange}
+        setPageSize={setPageSize}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+        pageSize={pageSize}
+      />
+    </>
+  )
 }
 
 export default SQAAuditReportFilterOptions
