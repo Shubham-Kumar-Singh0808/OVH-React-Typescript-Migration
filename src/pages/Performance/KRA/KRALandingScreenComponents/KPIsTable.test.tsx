1b13304550584b2c007acbcb22f648d100c8ac4d
Sai Kiran Banothu <saikiran.banothu@raybusiness.onmicrosoft.com>
2023-03-03 16:25:57 +0530

    fixed failed test in KPIsTable.test.tsx

--- src/pages/Performance/KRA/KRALandingScreenComponents/KPIsTable.test.tsx
+++ src/pages/Performance/KRA/KRALandingScreenComponents/KPIsTable.test.tsx
@@ -86,7 +86,7 @@ describe('KPI Table', () => {
       const name = screen.getByTestId('kpi-Name-1')
       const description = screen.getByTestId('kpi-description-2')
       expect(name).toHaveTextContent('Learn new technologies or...')
-      expect(description).toHaveTextContent('<span>This testimonial of...')
+      expect(description).toHaveTextContent('This testimonial of...')
 
       userEvent.click(description)
       const modalCnt = screen.getByTestId(modalContentId)
