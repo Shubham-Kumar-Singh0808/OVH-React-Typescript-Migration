f65b56c13a62befcf2d78cda5630e18af16d9366
Jyothika Devi <jyothikadevi.goru@raybusiness.onmicrosft.com>
2023-04-05 16:23:16 +0530

    Added User roles

--- src/pages/Performance/PipList/EmployeePipList/EmployeePipList.tsx
+++ src/pages/Performance/PipList/EmployeePipList/EmployeePipList.tsx
@@ -220,6 +220,18 @@ const EmployeePipList = (): JSX.Element => {
     }
   }
 
+  const userAccessToFeatures = useTypedSelector(
+    reduxServices.userAccessToFeatures.selectors.userAccessToFeatures,
+  )
+
+  const HierarchyUserAccess = userAccessToFeatures?.find(
+    (feature) => feature.name === 'Hierarchy PIP List',
+  )
+
+  const IndividualUserAccess = userAccessToFeatures?.find(
+    (feature) => feature.name === 'Individual PIP List',
+  )
+
   return (
     <>
       {toggle === '' && (
@@ -229,208 +241,230 @@ const EmployeePipList = (): JSX.Element => {
           CBodyClassName="ps-0 pe-0"
           CFooterClassName="d-none"
         >
-          <CRow className="employeeAllocation-form  mt-4">
-            <CCol sm={2} md={1} className="text-end">
-              <CFormLabel className="mt-2">Select:</CFormLabel>
-            </CCol>
-            <CCol sm={2}>
-              <CFormSelect
-                aria-label="Default select example"
-                size="sm"
-                id="selectDate"
-                data-testid="form-select1"
-                name="selectDate"
-                value={selectDate}
-                onChange={(e) => {
-                  setSelectDate(e.target.value)
-                  if (!localStorage.getItem('fmonth')) {
-                    localStorage.setItem('fmonth', e.target.value)
-                  }
-                }}
-              >
-                <option value="Today">Today</option>
-                <option value="Yesterday">Yesterday</option>
-                <option value="This Week">This Week</option>
-                <option value="Last Week">Last Week</option>
-                <option value="Last Month">Last Month</option>
-                <option value="Current Month">Current Month</option>
-                <option value="Custom">Custom</option>
-              </CFormSelect>
-            </CCol>
-            <CCol sm={12} md={9}>
-              <EmployeePipListOptions
-                selectDate={selectDate}
-                paginationRange={paginationRange}
-                setPageSize={setPageSize}
-                setCurrentPage={setCurrentPage}
-                currentPage={currentPage}
-                pageSize={pageSize}
-                fromDate={fromDate}
-                toDate={toDate}
-                searchInput={searchInput}
-                searchByAdded={searchByAdded}
-                searchByEmployee={searchByEmployee}
-                setToggle={setToggle}
-                setSelectDate={setSelectDate}
-                setFromDate={setFromDate}
-                setToDate={setToDate}
-              />
-            </CCol>
-            {selectDate === 'Custom' ? (
-              <>
-                <CCol sm={2} md={1} className="text-end">
-                  <CFormLabel className="mt-1">
-                    From:
-                    <span className={showIsRequired(fromDate as string)}>
-                      *
-                    </span>
-                  </CFormLabel>
-                </CCol>
-                <CCol sm={2}>
-                  <ReactDatePicker
-                    id="fromDate"
-                    data-testid="leaveApplyFromDate"
-                    className="form-control form-control-sm sh-date-picker form-control-not-allowed"
-                    showMonthDropdown
-                    showYearDropdown
-                    autoComplete="off"
-                    dropdownMode="select"
-                    dateFormat="dd/mm/yy"
-                    placeholderText="dd/mm/yyyy"
-                    name="fromDate"
-                    maxDate={disableAfterDate}
-                    value={fromDate}
-                    onChange={(date: Date) => onHandleFromDatePicker(date)}
-                  />
-                </CCol>
-                <CCol sm={2} md={1} className="text-end">
-                  <CFormLabel className="mt-1">
-                    To:
-                    <span className={showIsRequired(toDate as string)}>*</span>
-                  </CFormLabel>
-                </CCol>
-                <CCol sm={2}>
-                  <ReactDatePicker
-                    id="toDate"
-                    data-testid="leaveApprovalFromDate"
-                    className="form-control form-control-sm sh-date-picker form-control-not-allowed"
-                    showMonthDropdown
-                    autoComplete="off"
-                    showYearDropdown
-                    dropdownMode="select"
-                    dateFormat="dd/mm/yy"
-                    placeholderText="dd/mm/yyyy"
-                    name="toDate"
-                    maxDate={disableAfterDate}
-                    value={toDate}
-                    onChange={(date: Date) => onHandleToDatePicker(date)}
-                  />
-                  {dateError && (
-                    <span className="text-danger" data-testid="errorMessage">
-                      <b>To date should be greater than From date</b>
-                    </span>
+          <>
+            {!IndividualUserAccess?.viewaccess &&
+              HierarchyUserAccess?.viewaccess && (
+                <CRow className="employeeAllocation-form  mt-4">
+                  <CCol sm={2} md={1} className="text-end">
+                    <CFormLabel className="mt-2">Select:</CFormLabel>
+                  </CCol>
+                  <CCol sm={2}>
+                    <CFormSelect
+                      aria-label="Default select example"
+                      size="sm"
+                      id="selectDate"
+                      data-testid="form-select1"
+                      name="selectDate"
+                      value={selectDate}
+                      onChange={(e) => {
+                        setSelectDate(e.target.value)
+                        if (!localStorage.getItem('fmonth')) {
+                          localStorage.setItem('fmonth', e.target.value)
+                        }
+                      }}
+                    >
+                      <option value="Today">Today</option>
+                      <option value="Yesterday">Yesterday</option>
+                      <option value="This Week">This Week</option>
+                      <option value="Last Week">Last Week</option>
+                      <option value="Last Month">Last Month</option>
+                      <option value="Current Month">Current Month</option>
+                      <option value="Custom">Custom</option>
+                    </CFormSelect>
+                  </CCol>
+                  <CCol sm={12} md={9}>
+                    <EmployeePipListOptions
+                      selectDate={selectDate}
+                      paginationRange={paginationRange}
+                      setPageSize={setPageSize}
+                      setCurrentPage={setCurrentPage}
+                      currentPage={currentPage}
+                      pageSize={pageSize}
+                      fromDate={fromDate}
+                      toDate={toDate}
+                      searchInput={searchInput}
+                      searchByAdded={searchByAdded}
+                      searchByEmployee={searchByEmployee}
+                      setToggle={setToggle}
+                      setSelectDate={setSelectDate}
+                      setFromDate={setFromDate}
+                      setToDate={setToDate}
+                    />
+                  </CCol>
+                  {selectDate === 'Custom' ? (
+                    <>
+                      <CCol sm={2} md={1} className="text-end">
+                        <CFormLabel className="mt-1">
+                          From:
+                          <span className={showIsRequired(fromDate as string)}>
+                            *
+                          </span>
+                        </CFormLabel>
+                      </CCol>
+                      <CCol sm={2}>
+                        <ReactDatePicker
+                          id="fromDate"
+                          data-testid="leaveApplyFromDate"
+                          className="form-control form-control-sm sh-date-picker form-control-not-allowed"
+                          showMonthDropdown
+                          showYearDropdown
+                          autoComplete="off"
+                          dropdownMode="select"
+                          dateFormat="dd/mm/yy"
+                          placeholderText="dd/mm/yyyy"
+                          name="fromDate"
+                          maxDate={disableAfterDate}
+                          value={fromDate}
+                          onChange={(date: Date) =>
+                            onHandleFromDatePicker(date)
+                          }
+                        />
+                      </CCol>
+                      <CCol sm={2} md={1} className="text-end">
+                        <CFormLabel className="mt-1">
+                          To:
+                          <span className={showIsRequired(toDate as string)}>
+                            *
+                          </span>
+                        </CFormLabel>
+                      </CCol>
+                      <CCol sm={2}>
+                        <ReactDatePicker
+                          id="toDate"
+                          data-testid="leaveApprovalFromDate"
+                          className="form-control form-control-sm sh-date-picker form-control-not-allowed"
+                          showMonthDropdown
+                          autoComplete="off"
+                          showYearDropdown
+                          dropdownMode="select"
+                          dateFormat="dd/mm/yy"
+                          placeholderText="dd/mm/yyyy"
+                          name="toDate"
+                          maxDate={disableAfterDate}
+                          value={toDate}
+                          onChange={(date: Date) => onHandleToDatePicker(date)}
+                        />
+                        {dateError && (
+                          <span
+                            className="text-danger"
+                            data-testid="errorMessage"
+                          >
+                            <b>To date should be greater than From date</b>
+                          </span>
+                        )}
+                      </CCol>
+                    </>
+                  ) : (
+                    <></>
                   )}
-                </CCol>
-              </>
-            ) : (
-              <></>
-            )}
-          </CRow>
-          <CRow className="mt-4 mb-4">
-            <CCol sm={9} md={{ offset: 3 }}>
-              <CButton
-                className="cursor-pointer"
-                color="success btn-ovh me-1"
-                data-testid="view-btn"
-                onClick={viewButtonHandler}
-                disabled={
-                  (selectDate === 'Custom' &&
-                    !(fromDate !== '' && toDate !== '')) ||
-                  dateError
-                }
-              >
-                View
-              </CButton>
-              <CButton
-                className="cursor-pointer"
-                disabled={false}
-                color="warning btn-ovh me-1"
-                onClick={clearButtonHandler}
-              >
-                Clear
-              </CButton>
-            </CCol>
-          </CRow>
-          <CRow className="justify-content-end">
-            <CCol sm={3}>
-              <label className="search_emp">
-                <CFormCheck
-                  className="pt-2"
-                  data-testid="ch-searchByEmployee"
-                  id="searchByEmployee"
-                  name="searchByEmployee"
-                  checked={searchByEmployee}
-                  onChange={(e) => setSearchByEmployee(e.target.checked)}
-                />
-                <b>Search by Employee Name</b>
-              </label>
-              <label className="search_emp">
-                <CFormCheck
-                  className="pt-2"
-                  data-testid="ch-searchByAdded"
-                  id="searchByAdded"
-                  name="searchByAdded"
-                  checked={searchByAdded}
-                  onChange={(e) => setSearchByAdded(e.target.checked)}
-                />
-                <b>Search by Added by Name</b>
-              </label>
-            </CCol>
-          </CRow>
-          <CRow className="gap-2 d-md-flex justify-content-md-end">
-            <CCol sm={3} md={3}>
-              <CInputGroup className="global-search me-0">
-                <CFormInput
-                  disabled={!isMultiSearchBtn}
-                  data-testid="searchField"
-                  placeholder="Employee Search"
-                  aria-label="Multiple Search"
-                  aria-describedby="button-addon2"
-                  value={searchInput?.replace(/^\s*/, '')}
-                  onChange={(e) => {
-                    setSearchInput(e.target.value)
-                  }}
-                  onKeyDown={handleSearchBtn}
-                />
-                <CButton
-                  disabled={!searchInput?.replace(/^\s*/, '')}
-                  data-testid="multi-search-btn"
-                  className="cursor-pointer"
-                  type="button"
-                  color="info"
-                  id="button-addon2"
-                  onClick={multiSearchBtnHandler}
-                >
-                  <i className="fa fa-search"></i>
-                </CButton>
-              </CInputGroup>
-            </CCol>
-          </CRow>
-          <EmployeePipListTable
-            paginationRange={paginationRange}
-            setPageSize={setPageSize}
-            setCurrentPage={setCurrentPage}
-            currentPage={currentPage}
-            pageSize={pageSize}
-            selectDate={selectDate}
-            setToggle={setToggle}
-            setSelectDate={setSelectDate}
-            setFromDate={setFromDate}
-            setToDate={setToDate}
-          />
+                </CRow>
+              )}
+
+            {!IndividualUserAccess?.viewaccess &&
+              HierarchyUserAccess?.viewaccess && (
+                <CRow className="mt-4 mb-4">
+                  <CCol sm={9} md={{ offset: 3 }}>
+                    <CButton
+                      className="cursor-pointer"
+                      color="success btn-ovh me-1"
+                      data-testid="view-btn"
+                      onClick={viewButtonHandler}
+                      disabled={
+                        (selectDate === 'Custom' &&
+                          !(fromDate !== '' && toDate !== '')) ||
+                        dateError
+                      }
+                    >
+                      View
+                    </CButton>
+                    <CButton
+                      className="cursor-pointer"
+                      disabled={false}
+                      color="warning btn-ovh me-1"
+                      onClick={clearButtonHandler}
+                    >
+                      Clear
+                    </CButton>
+                  </CCol>
+                </CRow>
+              )}
+            {!IndividualUserAccess?.viewaccess &&
+              HierarchyUserAccess?.viewaccess && (
+                <CRow className="justify-content-end">
+                  <CCol sm={3}>
+                    <label className="search_emp">
+                      <CFormCheck
+                        className="pt-2"
+                        data-testid="ch-searchByEmployee"
+                        id="searchByEmployee"
+                        name="searchByEmployee"
+                        checked={searchByEmployee}
+                        onChange={(e) => setSearchByEmployee(e.target.checked)}
+                      />
+                      <b>Search by Employee Name</b>
+                    </label>
+                    <label className="search_emp">
+                      <CFormCheck
+                        className="pt-2"
+                        data-testid="ch-searchByAdded"
+                        id="searchByAdded"
+                        name="searchByAdded"
+                        checked={searchByAdded}
+                        onChange={(e) => setSearchByAdded(e.target.checked)}
+                      />
+                      <b>Search by Added by Name</b>
+                    </label>
+                  </CCol>
+                </CRow>
+              )}
+            {!IndividualUserAccess?.viewaccess &&
+              HierarchyUserAccess?.viewaccess && (
+                <CRow className="gap-2 d-md-flex justify-content-md-end">
+                  <CCol sm={3} md={3}>
+                    <CInputGroup className="global-search me-0">
+                      <CFormInput
+                        disabled={!isMultiSearchBtn}
+                        data-testid="searchField"
+                        placeholder="Employee Search"
+                        aria-label="Multiple Search"
+                        aria-describedby="button-addon2"
+                        value={searchInput?.replace(/^\s*/, '')}
+                        onChange={(e) => {
+                          setSearchInput(e.target.value)
+                        }}
+                        onKeyDown={handleSearchBtn}
+                      />
+                      <CButton
+                        disabled={!searchInput?.replace(/^\s*/, '')}
+                        data-testid="multi-search-btn"
+                        className="cursor-pointer"
+                        type="button"
+                        color="info"
+                        id="button-addon2"
+                        onClick={multiSearchBtnHandler}
+                      >
+                        <i className="fa fa-search"></i>
+                      </CButton>
+                    </CInputGroup>
+                  </CCol>
+                </CRow>
+              )}
+            <EmployeePipListTable
+              paginationRange={paginationRange}
+              setPageSize={setPageSize}
+              setCurrentPage={setCurrentPage}
+              currentPage={currentPage}
+              pageSize={pageSize}
+              selectDate={selectDate}
+              setToggle={setToggle}
+              setSelectDate={setSelectDate}
+              setFromDate={setFromDate}
+              setToDate={setToDate}
+            />
+          </>
         </OCard>
       )}
-      {toggle === 'addPIP' && (
+      {toggle === 'addPIP' && !IndividualUserAccess?.viewaccess && (
         <AddEmployeePipList
           pageSize={pageSize}
           searchByAdded={searchByAdded}
