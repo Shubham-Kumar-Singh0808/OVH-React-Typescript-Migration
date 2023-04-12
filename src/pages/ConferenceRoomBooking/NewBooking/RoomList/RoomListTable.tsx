3437ff5b26de62382d143da5df5abfd1fa0e51c5
Jyothika Devi <jyothikadevi.goru@raybusiness.onmicrosft.com>
2023-04-12 16:15:08 +0530

    Added Delete Toast

--- src/pages/ConferenceRoomBooking/NewBooking/RoomList/RoomListTable.tsx
+++ src/pages/ConferenceRoomBooking/NewBooking/RoomList/RoomListTable.tsx
@@ -76,14 +76,29 @@ const RoomListTable = ({
     setSelectedRoomName(roomName)
   }
 
+  const deleteFailedToast = (
+    <OToast
+      toastMessage="Already meeting was booked in this room, so you cannot delete this room"
+      toastColor="danger"
+      data-testid="failedToast"
+    />
+  )
+
   const confirmDeleteRoom = async () => {
     setIsDeleteModalVisible(false)
-    await dispatch(reduxServices.roomLists.deleteRoom(deleteLocationId))
-
-    dispatch(reduxServices.roomLists.getMeetingRooms())
-    dispatch(reduxServices.app.actions.addToast(deletedToastElement))
+    const deleteRoomResult = await dispatch(
+      reduxServices.roomLists.deleteRoom(deleteLocationId),
+    )
+    if (reduxServices.roomLists.deleteRoom.fulfilled.match(deleteRoomResult)) {
+      dispatch(reduxServices.roomLists.getMeetingRooms())
+      dispatch(reduxServices.app.actions.addToast(deletedToastElement))
+    } else if (
+      reduxServices.roomLists.deleteRoom.rejected.match(deleteRoomResult) &&
+      deleteRoomResult.payload === 500
+    ) {
+      dispatch(reduxServices.app.actions.addToast(deleteFailedToast))
+    }
   }
-
   const tableHeaderToggleCell = {
     width: '9%',
     scope: 'col',
