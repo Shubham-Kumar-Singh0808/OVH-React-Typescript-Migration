1467cee42a1cbc976747d0b614bb1500e8692a74
Jyothika Devi <jyothikadevi.goru@raybusiness.onmicrosft.com>
2023-05-08 15:29:49 +0530

    Added pagination for Notification tab

--- src/pages/Notifications/Notifications.tsx
+++ src/pages/Notifications/Notifications.tsx
@@ -1,27 +1,67 @@
 import React, { useEffect } from 'react'
-import { CRow, CTableDataCell, CTableRow } from '@coreui/react-pro'
+import { CCol, CRow, CTableDataCell, CTableRow } from '@coreui/react-pro'
 import OCard from '../../components/ReusableComponent/OCard'
 import { reduxServices } from '../../reducers/reduxServices'
 import { useAppDispatch, useTypedSelector } from '../../stateStore'
+import OPageSizeSelect from '../../components/ReusableComponent/OPageSizeSelect'
+import OPagination from '../../components/ReusableComponent/OPagination'
+import { usePagination } from '../../middleware/hooks/usePagination'
 
 const Notifications = (): JSX.Element => {
   const dispatch = useAppDispatch()
+
   const employeeId = useTypedSelector(
     reduxServices.authentication.selectors.selectEmployeeId,
   )
   const notificationAlerts = useTypedSelector(
     reduxServices.notification.selectors.notificationAlerts,
   )
+  const listSize = useTypedSelector(
+    reduxServices.notification.selectors.listSize,
+  )
+
+  const totalNoOfRecords = notificationAlerts?.length
+    ? `Total Records: ${listSize}`
+    : `No Records found...`
+
+  const CurrentPage = useTypedSelector(
+    reduxServices.app.selectors.selectCurrentPage,
+  )
+
+  useEffect(() => {
+    if (CurrentPage) {
+      setCurrentPage(CurrentPage)
+    }
+  }, [CurrentPage])
+
+  const {
+    paginationRange,
+    setPageSize,
+    setCurrentPage,
+    currentPage,
+    pageSize,
+  } = usePagination(listSize, 20)
+
+  const handlePageSize = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setPageSize(Number(event.target.value))
+    setCurrentPage(1)
+    dispatch(reduxServices.app.actions.setPersistCurrentPage(1))
+  }
 
   useEffect(() => {
     dispatch(
       reduxServices.notification.getAllAlerts({
         employeeId: Number(employeeId),
-        endIndex: 20,
-        startIndex: 0,
+        startIndex: pageSize * (currentPage - 1),
+        endIndex: pageSize * currentPage,
       }),
     )
-  }, [dispatch])
+  }, [currentPage, dispatch, pageSize])
+
+  const getItemNumber = (index: number) => {
+    return (currentPage - 1) * pageSize + index + 1
+  }
+
   return (
     <>
       <OCard
@@ -35,11 +75,41 @@ const Notifications = (): JSX.Element => {
             notificationAlerts?.map((location, index) => {
               return (
                 <CTableRow key={index}>
-                  <CTableDataCell scope="row">{index}</CTableDataCell>
+                  <CTableDataCell scope="row">
+                    {getItemNumber(index)}
+                  </CTableDataCell>
                   <CTableDataCell>{location.msg}</CTableDataCell>
                 </CTableRow>
               )
             })}
+        </CRow>
+        <CRow>
+          <CCol xs={4}>
+            <p className="mt-2">
+              <strong>{totalNoOfRecords}</strong>
+            </p>
+          </CCol>
+          <CCol xs={3}>
+            {listSize > 20 && (
+              <OPageSizeSelect
+                handlePageSizeSelectChange={handlePageSize}
+                options={[20, 40, 60, 80, 100]}
+                selectedPageSize={pageSize}
+              />
+            )}
+          </CCol>
+          {listSize > 20 && (
+            <CCol
+              xs={5}
+              className="d-grid gap-1 d-md-flex justify-content-md-end"
+            >
+              <OPagination
+                currentPage={currentPage}
+                pageSetter={setCurrentPage}
+                paginationRange={paginationRange}
+              />
+            </CCol>
+          )}
         </CRow>
       </OCard>
     </>
