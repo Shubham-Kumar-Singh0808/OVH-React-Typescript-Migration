ae540bc4307a6130ed3bb853090be45b0ceb3a22
Jyothika Devi <jyothikadevi.goru@raybusiness.onmicrosft.com>
2023-05-08 20:29:19 +0530

    Added files testcases

--- src/pages/Notifications/Notifications.test.tsx
+++ src/pages/Notifications/Notifications.test.tsx
@@ -1,9 +1,11 @@
 import '@testing-library/jest-dom'
 import React from 'react'
+import userEvent from '@testing-library/user-event'
 import Notifications from './Notifications'
-import { render, screen } from '../../test/testUtils'
+import { render, screen, waitFor } from '../../test/testUtils'
 import { ApiLoadingState } from '../../middleware/api/apiList'
 import { UpdateList } from '../../types/Notifications/notificationTypes'
+import { mockNotificationAlertsData } from '../../test/data/notificationsData'
 
 describe('Notifications without data', () => {
   beforeEach(() => {
@@ -20,5 +22,37 @@ describe('Notifications without data', () => {
   })
   test('should be able to render  Notifications  Title', () => {
     expect(screen.getByText('Notifications')).toBeInTheDocument()
+  })
+})
+
+describe('Notifications with data', () => {
+  beforeEach(() => {
+    render(<Notifications />, {
+      preloadedState: {
+        notification: {
+          notificationAlerts: mockNotificationAlertsData.alertsList,
+          isLoading: ApiLoadingState.succeeded,
+          listSize: 0,
+          updateAlertsList: {} as UpdateList,
+        },
+      },
+    })
+  })
+  test('should render first page data only', () => {
+    waitFor(() => {
+      userEvent.click(screen.getByText('Next >', { exact: true }))
+
+      expect(screen.getByText('« First')).not.toHaveAttribute('disabled')
+      expect(screen.getByText('< Prev')).not.toHaveAttribute('disabled')
+    })
+  })
+
+  test('should disable first and prev in pagination if first page', () => {
+    waitFor(() => {
+      expect(screen.getByText('« First')).toHaveAttribute('disabled')
+      expect(screen.getByText('< Prev')).toHaveAttribute('disabled')
+      expect(screen.getByText('Next >')).not.toHaveAttribute('disabled')
+      expect(screen.getByText('Last »')).not.toHaveAttribute('disabled')
+    })
   })
 })
