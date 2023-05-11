668aca7f8c7383b878339a889717ef608ca88af4
Jyothika Devi <jyothikadevi.goru@raybusiness.onmicrosft.com>
2023-05-12 01:17:21 +0530

    Added testcases

--- src/pages/Recruitment/JobOpenings/JobOpenings.test.tsx
+++ src/pages/Recruitment/JobOpenings/JobOpenings.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom'
 import React from 'react'
+import userEvent from '@testing-library/user-event'
 import JobOpenings from './JobOpenings'
 import { render, screen } from '../../../test/testUtils'
 import { ApiLoadingState } from '../../../middleware/api/apiList'
@@ -19,5 +20,20 @@ describe('Job Openings without data', () => {
   })
   test('should be able to render  Job Openings  Title', () => {
     expect(screen.getByText('Job Openings')).toBeInTheDocument()
+  })
+  test('should render add PIP component with out crashing', () => {
+    expect(screen.getByRole('button', { name: 'Add' })).toBeInTheDocument()
+  })
+  test('should able to click Add Button', () => {
+    const addBtnElement = screen.getByRole('button', {
+      name: 'Add',
+    })
+    expect(addBtnElement).toBeEnabled()
+    userEvent.click(addBtnElement)
+  })
+  test('upon providing search text and clicking on search button it should call mockSetMultiSearchValue function', () => {
+    const searchInput = screen.getByTestId('searchField')
+    userEvent.type(searchInput, 'Admin  Rbt')
+    userEvent.click(screen.getByTestId('multi-search-btn'))
   })
 })
