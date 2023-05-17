5d27edafbf5a5974bdcac37be73f0916535cd1f1
Anusha Singathi <ASingathi@ABS-CBN.COM>
2023-05-17 20:36:25 +0530

    resolved issues

--- src/pages/Assets/ManufacturerList/ManufacturerListTable.test.tsx
+++ src/pages/Assets/ManufacturerList/ManufacturerListTable.test.tsx
@@ -1,27 +1,61 @@
+import '@testing-library/jest-dom'
 import React from 'react'
+import userEvent from '@testing-library/user-event'
 import ManufacturerListTable from './ManufacturerListTable'
-import { render, screen } from '../../../test/testUtils'
+import { render, screen, waitFor } from '../../../test/testUtils'
+import { ApiLoadingState } from '../../../middleware/api/apiList'
+import { GetAllManufacturerName } from '../../../types/Assets/ManufacturerList/ManufacturerType'
+import { mockManufacturerData } from '../../../test/data/ManufacturerListData'
 
-// Mock the redux store and dependencies as needed
-
-describe('Manufacturer Component Testing', () => {
-  test('renders ManufacturerListTable component', () => {
-    const paginationRange = [1, 2, 3]
-    const setPageSize = jest.fn()
-    const setCurrentPage = jest.fn()
-    const currentPage = 1
-    const pageSize = 20
-
+const mockSetData = jest.fn()
+describe('Job Openings without data', () => {
+  beforeEach(() => {
     render(
       <ManufacturerListTable
-        paginationRange={paginationRange}
-        setPageSize={setPageSize}
-        setCurrentPage={setCurrentPage}
-        currentPage={currentPage}
-        pageSize={pageSize}
+        paginationRange={[]}
+        currentPage={0}
+        setCurrentPage={mockSetData}
+        pageSize={0}
+        setPageSize={mockSetData}
       />,
+      {
+        preloadedState: {
+          manufacturerList: {
+            isLoading: ApiLoadingState.succeeded,
+            manufacturerDetails: mockManufacturerData,
+            getAllManufacturerName: {} as GetAllManufacturerName,
+            listSize: 0,
+          },
+        },
+      },
     )
+  })
+  test('should render first page data only', () => {
+    waitFor(() => {
+      userEvent.click(screen.getByText('Next >', { exact: true }))
 
+      expect(screen.getByText('« First')).not.toHaveAttribute('disabled')
+      expect(screen.getByText('< Prev')).not.toHaveAttribute('disabled')
+    })
+  })
+
+  test('should disable first and prev in pagination if first page', () => {
+    waitFor(() => {
+      expect(screen.getByText('« First')).toHaveAttribute('disabled')
+      expect(screen.getByText('< Prev')).toHaveAttribute('disabled')
+      expect(screen.getByText('Next >')).not.toHaveAttribute('disabled')
+      expect(screen.getByText('Last »')).not.toHaveAttribute('disabled')
+    })
+  })
+
+  test('should render  component with data', () => {
+    expect(screen.getByText('Stagging Test')).toBeInTheDocument()
+    expect(screen.getByText('Pavani')).toBeInTheDocument()
+  })
+  test('Should be able to see table titles', () => {
+    expect(screen.getByText('#')).toBeInTheDocument()
     expect(screen.getByText('Manufacturer Name')).toBeInTheDocument()
+    expect(screen.getByText('Last Updated by')).toBeInTheDocument()
+    expect(screen.getByText('Actions')).toBeInTheDocument()
   })
 })
